b42c0929a33d92ce39294e371f9752b124ce093f
Max <maxgrasbeck@gmail.com>
2017-11-13 12:06:04 +0200

    Refactor tenant model

--- leasing/models/tenant.py
+++ leasing/models/tenant.py
@@ -7,13 +7,35 @@ from leasing.models.mixins import TimestampedModelMixin
 
 
 class Tenant(TimestampedModelMixin):
-    lease = models.ForeignKey(Lease, related_name="tenants", on_delete=models.CASCADE)
-    contact = models.ForeignKey(Contact, related_name="tenants", on_delete=models.CASCADE)
-    contact_contact = models.ForeignKey(Contact, related_name="tenant_contacts", null=True, blank=True,
-                                        on_delete=models.CASCADE)
-    billing_contact = models.ForeignKey(Contact, related_name="tenant_billing_contacts", null=True, blank=True,
-                                        on_delete=models.CASCADE)
-    share = models.DecimalField(verbose_name=_("Share of the rent"), max_digits=7, decimal_places=6)
+    lease = models.ForeignKey(
+        Lease,
+        related_name="tenants",
+        on_delete=models.CASCADE,
+    )
+    contact = models.ForeignKey(
+        Contact,
+        related_name="tenants",
+        on_delete=models.CASCADE,
+    )
+    contact_contact = models.ForeignKey(
+        Contact,
+        related_name="tenant_contacts",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+    billing_contact = models.ForeignKey(
+        Contact,
+        related_name="tenant_billing_contacts",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+    share = models.DecimalField(
+        verbose_name=_("Share of the rent"),
+        max_digits=7,
+        decimal_places=6,
+    )
 
     def get_billing_contact(self):
         return self.billing_contact if self.billing_contact else self.contact
