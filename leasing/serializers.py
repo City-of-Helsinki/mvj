5cb11bb224df0d18cc9ea8d70b905ff2c52807ce
Max <maxgrasbeck@gmail.com>
2017-11-21 16:47:46 +0200

    Add start and end date validation

--- leasing/serializers.py
+++ leasing/serializers.py
@@ -1,5 +1,7 @@
+from django.utils.translation import ugettext_lazy as _
 from enumfields.drf import EnumSupportSerializerMixin
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from leasing.models import Lease
 
@@ -7,6 +9,16 @@ from leasing.models import Lease
 class LeaseSerializer(EnumSupportSerializerMixin, serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     identifier = serializers.ReadOnlyField(source='identifier_string')
+
+    def validate(self, data):
+        start_date = data['start_date']
+        end_date = data['end_date']
+
+        if start_date is not None and end_date is not None:
+            if start_date > end_date:
+                raise ValidationError(_('Start date must be before end date.'))
+
+        return data
 
     class Meta:
         model = Lease
