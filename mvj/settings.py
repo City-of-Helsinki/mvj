f69db7e0abc61f8754fa213e989530d449642f2c
Max <maxgrasbeck@gmail.com>
2017-11-22 11:22:15 +0200

    Disable authentication for demonstration purposes

--- mvj/settings.py
+++ mvj/settings.py
@@ -126,7 +126,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
