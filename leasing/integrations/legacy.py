78ffd0933108b4397646482c18f6de211b8e5049
Max <maxgrasbeck@gmail.com>
2017-11-24 13:46:25 +0200

    Add legacy importer
    Add legacy importer script that connects to the legacy oracle database
    and imports data from a table and creates lease objects. Supports adding
    more importer/table-pairs.

---
+++ leasing/integrations/legacy.py
@@ -0,0 +1,57 @@
+from django.conf import settings
+
+import cx_Oracle
+from leasing.models import Lease
+
+
+def get_cursor():
+    dsn_tns = cx_Oracle.makedsn(
+        settings.LEGACY_HOST,
+        settings.LEGACY_PORT,
+        settings.LEGACY_SERVICES,
+    )
+
+    connection = cx_Oracle.connect(
+        user=settings.LEGACY_USER,
+        password=settings.LEGACY_PASSWORD,
+        dsn=dsn_tns,
+    )
+    return connection.cursor()
+
+
+def import_table(importer, table_name):
+    cursor = get_cursor()
+    cursor.execute("SELECT count(*) FROM %s" % table_name)
+    total = cursor.fetchone()[0]
+    cursor.execute("SELECT * FROM %s" % table_name)
+    i = 0
+
+    for row in cursor:
+        importer(row)
+        i += 1
+        percent = round(i/total*100, 1)
+        print('Importing', table_name, str(percent) + '%\r', end='')
+    print('')
+
+
+def import_lease(row):
+    lease, created = Lease.objects.get_or_create(
+        type=row[0],
+        municipality=row[1],
+        district=row[2],
+        # row[3] skipped on purpose
+        sequence=row[4],
+        start_date=row[15],
+        end_date=row[16],
+    )
+    return lease
+
+
+IMPORTERS_AND_TABLE_NAMES = [
+    (import_lease, 'vuokraus'),
+]
+
+
+def run_import():
+    for method, table_name in IMPORTERS_AND_TABLE_NAMES:
+        import_table(method, table_name)
