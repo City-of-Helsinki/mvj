9626c09a822b39e910f38d61c51d0ee9f6c8395b
Max <maxgrasbeck@gmail.com>
2018-02-19 13:03:35 +0200

    Add plot
    Plots are actual land, as opposed to plan plots that are just plans, not
    the actual land.

--- leasing/models/__init__.py
+++ leasing/models/__init__.py
@@ -2,11 +2,14 @@ from .asset import Asset
 from .land_area import LandArea
 from .lease import Lease, LeaseStatus
 from .plan_plot import PlanPlot, PlanPlotState, PlanPlotType, PlanPlotUsagePurpose
+from .plot import Plot, PlotExplanation
 
 __all__ = [
     'Asset',
     'Lease',
     'LeaseStatus',
+    'Plot',
+    'PlotExplanation',
     'PlanPlot',
     'PlanPlotState',
     'PlanPlotType',
