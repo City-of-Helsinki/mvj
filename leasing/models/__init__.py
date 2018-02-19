95387472afeb47dc6add3743be278a296ac2645c
Max <maxgrasbeck@gmail.com>
2018-02-19 16:12:11 +0200

    Add tenant to __init__

--- leasing/models/__init__.py
+++ leasing/models/__init__.py
@@ -1,8 +1,11 @@
 from .asset import Asset
 from .lease import Lease, LeaseStatus
+from .tenant import MainTenant, SubTenant
 
 __all__ = [
     'Asset',
     'Lease',
     'LeaseStatus',
+    'MainTenant',
+    'SubTenant',
 ]
