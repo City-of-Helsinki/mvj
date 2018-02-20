e6f12e72def42744b86bfd314684c452dcadea36
Max <maxgrasbeck@gmail.com>
2018-02-20 10:55:11 +0200

    Add comment mixin

--- leasing/models/mixins.py
+++ leasing/models/mixins.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
+class CommentMixin(models.Model):
+    comment = models.TextField(
+        verbose_name=_("Comment"),
+    )
+
+    class Meta:
+        abstract = True
+
+
 class TimestampedModelMixin(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Time created"))
     modified_at = models.DateTimeField(auto_now=True, verbose_name=_("Time modified"))
