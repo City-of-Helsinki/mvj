23fa65b7af023af90a6ff4997f8cc24679bd841e
Mikko Keskinen <mikko.keskinen@anders.fi>
2018-03-20 14:16:18 +0200

    Add filter for Contact

--- leasing/filters.py
+++ leasing/filters.py
@@ -1,6 +1,14 @@
 from django_filters.rest_framework import FilterSet
 
-from leasing.models import Lease
+from .models import Contact, Lease
+
+
+class ContactFilter(FilterSet):
+    class Meta:
+        model = Contact
+        fields = ['first_name', 'last_name', 'is_business', 'business_name', 'business_id',
+                  'national_identification_number', 'customer_number', 'sap_customer_number', 'partner_code',
+                  'is_lessor']
 
 
 class LeaseFilter(FilterSet):
--- leasing/viewsets/contact.py
+++ leasing/viewsets/contact.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 
+from leasing.filters import ContactFilter
 from leasing.models import Contact
 from leasing.serializers.contact import ContactSerializer
 from leasing.viewsets.utils import AuditLogMixin
@@ -8,3 +9,4 @@ from leasing.viewsets.utils import AuditLogMixin
 class ContactViewSet(AuditLogMixin, viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
+    filter_class = ContactFilter
