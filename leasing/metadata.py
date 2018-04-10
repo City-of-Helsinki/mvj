c94e6f3d43211ea4eb76dde889724bd93f02accf
Mikko Keskinen <mikko.keskinen@anders.fi>
2018-04-10 11:42:49 +0300

    Add decimal_places and max_digits info for DecimalFields in the metadata

--- leasing/metadata.py
+++ leasing/metadata.py
@@ -1,10 +1,13 @@
 from django.utils.encoding import force_text
+from rest_framework.fields import DecimalField
 from rest_framework.metadata import SimpleMetadata
 
 
 class FieldsMetadata(SimpleMetadata):
     """Returns metadata for all the fields and the possible choices in the
-    serializer even when the fields are read only."""
+    serializer even when the fields are read only.
+
+    Additionally adds decimal_places and max_digits info for DecimalFields."""
 
     def determine_metadata(self, request, view):
         metadata = super().determine_metadata(request, view)
@@ -16,6 +19,10 @@ class FieldsMetadata(SimpleMetadata):
 
     def get_field_info(self, field):
         field_info = super().get_field_info(field)
+
+        if isinstance(field, DecimalField):
+            field_info['decimal_places'] = field.decimal_places
+            field_info['max_digits'] = field.max_digits
 
         if hasattr(field, 'choices'):
             field_info['choices'] = [{
