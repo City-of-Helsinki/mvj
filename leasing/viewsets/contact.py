6db62805afa78338de2fe9644940cab14f6a4595
Mikko Keskinen <mikko.keskinen@anders.fi>
2018-06-06 12:21:31 +0300

    Add ability to search contacts

--- leasing/viewsets/contact.py
+++ leasing/viewsets/contact.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
 
 from leasing.filters import ContactFilter
 from leasing.models import Contact
@@ -10,3 +11,5 @@ class ContactViewSet(AuditLogMixin, viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
     filter_class = ContactFilter
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter, )
+    search_fields = ('first_name', 'last_name', 'name', 'business_id', 'customer_number', 'sap_customer_number')
