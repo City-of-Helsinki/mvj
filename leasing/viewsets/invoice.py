74570b193302cc563c8f4f939fdbbaae3cdfdef0
Mikko Keskinen <mikko.keskinen@anders.fi>
2018-06-06 12:21:50 +0300

    Add select_related and prefetch_related

--- leasing/viewsets/invoice.py
+++ leasing/viewsets/invoice.py
@@ -10,6 +10,12 @@ class InvoiceViewSet(viewsets.ModelViewSet):
     serializer_class = InvoiceSerializer
     filter_class = InvoiceFilter
 
+    def get_queryset(self):
+        queryset = Invoice.objects.select_related('recipient').prefetch_related(
+            'rows', 'rows__tenant', 'rows__tenant__tenantcontact_set', 'rows__tenant__tenantcontact_set__contact')
+
+        return queryset
+
     def get_serializer_class(self):
         if self.action == 'create':
             return InvoiceCreateSerializer
