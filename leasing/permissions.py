8dd076c720d0271baf18c48eaeb9ab7657b6cf73
Mikko Keskinen <mikko.keskinen@anders.fi>
2019-01-08 15:48:47 +0200

    Add PerMethodPermission for checking custom view permissions

--- leasing/permissions.py
+++ leasing/permissions.py
@@ -1,7 +1,7 @@
-from rest_framework.permissions import DjangoModelPermissions
+from rest_framework import permissions
 
 
-class MvjDjangoModelPermissions(DjangoModelPermissions):
+class MvjDjangoModelPermissions(permissions.DjangoModelPermissions):
     """Customized Django REST Framework DjangoModelPermissions
     class that includes checking for the "view" permissions too.
     """
@@ -14,3 +14,45 @@ class MvjDjangoModelPermissions(DjangoModelPermissions):
         'PATCH': ['%(app_label)s.change_%(model_name)s'],
         'DELETE': ['%(app_label)s.delete_%(model_name)s'],
     }
+
+
+class PerMethodPermission(permissions.BasePermission):
+    """Per method permission check
+
+    Permission where the required permissions can be configured
+    per method. Returns True if no permissions are configured.
+    If multiple permissions listed, the user must have all of
+    the permissions."""
+
+    perms_map = {
+        'GET': [],
+        'POST': [],
+        'PUT': [],
+        'PATCH': [],
+        'DELETE': [],
+        'OPTIONS': [],
+    }
+
+    def get_required_permissions(self, method, view):
+        """Get required permissions for method and view combo
+
+        Given method and view, return the list of permission
+        codes that the user is required to have.
+        """
+        perms_map = self.perms_map.copy()
+
+        if hasattr(view, 'perms_map'):
+            perms_map.update(view.perms_map)
+
+        if method not in perms_map:
+            return []
+
+        return perms_map[method]
+
+    def has_permission(self, request, view):
+        if not request.user or not request.user.is_authenticated:
+            return False
+
+        required_perms = self.get_required_permissions(request.method, view)
+
+        return request.user.has_perms(required_perms)
