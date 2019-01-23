e6594544b03979e61ac94d8c7ff8a13b43ab9231
Mikko Keskinen <mikko.keskinen@anders.fi>
2019-01-23 13:50:30 +0200

    Check field permissions in Inspection serializer

--- leasing/serializers/inspection.py
+++ leasing/serializers/inspection.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
+from field_permissions.serializers import FieldPermissionsSerializerMixin
+
 from ..models import Inspection
 
 
-class InspectionSerializer(serializers.ModelSerializer):
+class InspectionSerializer(FieldPermissionsSerializerMixin, serializers.ModelSerializer):
     id = serializers.IntegerField(required=False)
 
     class Meta:
