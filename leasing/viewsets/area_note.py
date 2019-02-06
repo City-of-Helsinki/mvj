911c34a09ef5f55ff1ab7772da1caa30a84e7384
Mikko Keskinen <mikko.keskinen@anders.fi>
2019-02-06 16:42:06 +0200

    Enable searching AreaNotes by the note field

--- leasing/viewsets/area_note.py
+++ leasing/viewsets/area_note.py
@@ -1,3 +1,5 @@
+from rest_framework import filters
+
 from field_permissions.viewsets import FieldPermissionsViewsetMixin
 from leasing.models import AreaNote
 from leasing.serializers.area_note import AreaNoteCreateUpdateSerializer, AreaNoteSerializer
@@ -8,6 +10,8 @@ from .utils import AtomicTransactionModelViewSet, AuditLogMixin
 class AreaNoteViewSet(AuditLogMixin, FieldPermissionsViewsetMixin, AtomicTransactionModelViewSet):
     queryset = AreaNote.objects.all()
     serializer_class = AreaNoteSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('note',)
 
     def get_serializer_class(self):
         if self.action in ('create', 'update', 'partial_update', 'metadata'):
