6402377ff0289ea877bbbfa964cda9ae241074b2
Mikko Keskinen <keso@iki.fi>
2020-03-04 17:50:40 +0200

    Add support for custom serializer field in the output

--- leasing/report/serializers.py
+++ leasing/report/serializers.py
@@ -1,6 +1,8 @@
+import copy
 from collections import OrderedDict
 
 from rest_framework import serializers
+from rest_framework.fields import Field
 
 
 class ReportOutputSerializer(serializers.Serializer):
@@ -13,10 +15,23 @@ class ReportOutputSerializer(serializers.Serializer):
         super().__init__(*args, **kwargs)
         self.output_fields = output_fields
 
+    def update(self, instance, validated_data):
+        pass
+
+    def create(self, validated_data):
+        pass
+
     def get_fields(self):
         fields = OrderedDict()
 
         for field_name, field_attrs in self.output_fields.items():
+            field = field_attrs.get('serializer_field', None)
+
+            if field and isinstance(field, Field):
+                # The field must be a copy because DRF modifies the field
+                fields[field_name] = copy.deepcopy(field)
+                continue
+
             field_source = field_attrs.get('source', None)
 
             if field_source is None:
