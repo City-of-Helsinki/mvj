d7a69cdc0cb664bd5b457f21db5139b0ecb43fd5
Mikko Keskinen <keso@iki.fi>
2020-04-07 20:33:53 +0300

    Move excel file generation to ReportBase

--- leasing/report/renderers.py
+++ leasing/report/renderers.py
@@ -1,13 +1,7 @@
 import json
-from io import BytesIO
 
-import xlsxwriter
 from django.core.serializers.json import DjangoJSONEncoder
-from django.db.models import Model
-from django.utils.translation import ugettext as _
 from rest_framework import renderers
-
-from leasing.report.excel import ExcelRow, FormatType
 
 
 class XLSXRenderer(renderers.BaseRenderer):
@@ -16,105 +10,10 @@ class XLSXRenderer(renderers.BaseRenderer):
     charset = 'utf-8'
     render_style = 'binary'
 
-    def render(self, data, media_type=None, renderer_context=None):  # NOQA C901 'XLSXRenderer.render' is too complex
+    def render(self, data, media_type=None, renderer_context=None):
         # Return JSON response if the view is not a report or when an error occurred
         if renderer_context['view'].action != 'retrieve' or renderer_context['response'].status_code != 200:
             renderer_context['response']['Content-Type'] = 'application/json'
             return json.dumps(data, cls=DjangoJSONEncoder)
 
-        report = renderer_context['view'].report
-
-        output = BytesIO()
-        workbook = xlsxwriter.Workbook(output)
-        worksheet = workbook.add_worksheet()
-
-        formats = {
-            FormatType.BOLD: workbook.add_format({'bold': True}),
-            FormatType.DATE: workbook.add_format({'num_format': 'dd.mm.yyyy'}),
-            FormatType.MONEY: workbook.add_format({'num_format': '€#.00'}),
-            FormatType.BOLD_MONEY: workbook.add_format({'bold': True, 'num_format': '€#.00'}),
-        }
-
-        row_num = 0
-
-        # On the first row print the report name
-        worksheet.write(row_num, 0, str(report.name), formats[FormatType.BOLD])
-
-        # On the second row print the report description
-        row_num += 1
-        worksheet.write(row_num, 0, str(report.description))
-
-        # On the fourth row forwards print the input fields and their values
-        row_num += 2
-        for input_field_name, input_field in report.form.fields.items():
-            worksheet.write(row_num, 0, '{}:'.format(input_field.label), formats[FormatType.BOLD])
-            field_format = None
-            if input_field.__class__.__name__ == 'DateField':
-                field_format = formats[FormatType.DATE]
-
-            input_value = report.form.cleaned_data[input_field_name]
-            if hasattr(input_field, 'choices'):
-                for choice_value, choice_label in input_field.choices:
-                    if choice_value == input_value:
-                        input_value = str(choice_label)
-                        break
-
-            if isinstance(input_value, Model):
-                input_value = str(input_value)
-
-            worksheet.write(row_num, 1, input_value, field_format)
-            row_num += 1
-
-        # Set column widths
-        for index, field_name in enumerate(report.output_fields.keys()):
-            worksheet.set_column(index, index, report.get_output_field_attr(field_name, 'width', default=10))
-
-        # Labels from the first non-ExcelRow row
-        if report.automatic_excel_column_labels:
-            row_num += 1
-
-            lookup_row_num = 0
-            while lookup_row_num < len(data) and isinstance(data[lookup_row_num], ExcelRow):
-                lookup_row_num += 1
-
-            for index, field_name in enumerate(data[lookup_row_num].keys()):
-                field_label = report.get_output_field_attr(field_name, 'label', default=field_name)
-
-                worksheet.write(row_num, index, str(field_label), formats[FormatType.BOLD])
-
-        # The data itself
-        row_num += 1
-        first_data_row_num = row_num
-        for row in data:
-            if isinstance(row, dict):
-                column = 0
-                for field_name, field_value in row.items():
-                    field_format = None
-
-                    field_format_name = report.get_output_field_attr(field_name, 'format')
-
-                    if field_format_name == 'date':
-                        field_format = formats[FormatType.DATE]
-                    elif field_format_name == 'money':
-                        if field_value != 0:
-                            field_format = formats[FormatType.MONEY]
-                    elif field_format_name == 'boolean':
-                        if field_value:
-                            field_value = _('Yes')
-                        else:
-                            field_value = _('No')
-
-                    worksheet.write(row_num, column, field_value, field_format)
-                    column += 1
-            elif isinstance(row, ExcelRow):
-                for cell in row.cells:
-                    cell.set_row(row_num)
-                    cell.set_first_data_row_num(first_data_row_num)
-                    worksheet.write(row_num, cell.column, cell.get_value(),
-                                    formats[cell.get_format_type()] if cell.get_format_type() in formats else None)
-
-            row_num += 1
-
-        workbook.close()
-
-        return output.getvalue()
+        return renderer_context['view'].report.data_as_excel(data)
--- leasing/report/report_base.py
+++ leasing/report/report_base.py
@@ -1,7 +1,13 @@
+from io import BytesIO
+
+import xlsxwriter
+from django.db.models import Model
 from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
+from leasing.report.excel import ExcelRow, FormatType
 from leasing.report.forms import ReportFormBase
 from leasing.report.serializers import ReportOutputSerializer
 
@@ -101,3 +107,102 @@ class ReportBase:
             value = self.output_fields[field_name][attr_name]
 
         return value
+
+    def data_as_excel(self, data):  # NOQA C901 'ReportBase.data_as_excel' is too complex
+        report = self
+
+        output = BytesIO()
+        workbook = xlsxwriter.Workbook(output)
+        worksheet = workbook.add_worksheet()
+
+        formats = {
+            FormatType.BOLD: workbook.add_format({'bold': True}),
+            FormatType.DATE: workbook.add_format({'num_format': 'dd.mm.yyyy'}),
+            FormatType.MONEY: workbook.add_format({'num_format': '€#.00'}),
+            FormatType.BOLD_MONEY: workbook.add_format({'bold': True, 'num_format': '€#.00'}),
+        }
+
+        row_num = 0
+
+        # On the first row print the report name
+        worksheet.write(row_num, 0, str(report.name), formats[FormatType.BOLD])
+
+        # On the second row print the report description
+        row_num += 1
+        worksheet.write(row_num, 0, str(report.description))
+
+        # On the fourth row forwards print the input fields and their values
+        row_num += 2
+        for input_field_name, input_field in report.form.fields.items():
+            worksheet.write(row_num, 0, '{}:'.format(input_field.label), formats[FormatType.BOLD])
+            field_format = None
+            if input_field.__class__.__name__ == 'DateField':
+                field_format = formats[FormatType.DATE]
+
+            input_value = report.form.cleaned_data[input_field_name]
+            if hasattr(input_field, 'choices'):
+                for choice_value, choice_label in input_field.choices:
+                    if choice_value == input_value:
+                        input_value = str(choice_label)
+                        break
+
+            if isinstance(input_value, Model):
+                input_value = str(input_value)
+
+            worksheet.write(row_num, 1, input_value, field_format)
+            row_num += 1
+
+        # Set column widths
+        for index, field_name in enumerate(report.output_fields.keys()):
+            worksheet.set_column(index, index, report.get_output_field_attr(field_name, 'width', default=10))
+
+        # Labels from the first non-ExcelRow row
+        if report.automatic_excel_column_labels:
+            row_num += 1
+
+            lookup_row_num = 0
+            while lookup_row_num < len(data) and lookup_row_num in data and isinstance(data[lookup_row_num], ExcelRow):
+                lookup_row_num += 1
+
+            if lookup_row_num in data:
+                for index, field_name in enumerate(data[lookup_row_num].keys()):
+                    field_label = report.get_output_field_attr(field_name, 'label', default=field_name)
+
+                    worksheet.write(row_num, index, str(field_label), formats[FormatType.BOLD])
+
+        # The data itself
+        row_num += 1
+        first_data_row_num = row_num
+        for row in data:
+            if isinstance(row, dict):
+                column = 0
+                for field_name, field_value in row.items():
+                    field_format = None
+
+                    field_format_name = report.get_output_field_attr(field_name, 'format')
+
+                    if field_format_name == 'date':
+                        field_format = formats[FormatType.DATE]
+                    elif field_format_name == 'money':
+                        if field_value != 0:
+                            field_format = formats[FormatType.MONEY]
+                    elif field_format_name == 'boolean':
+                        if field_value:
+                            field_value = _('Yes')
+                        else:
+                            field_value = _('No')
+
+                    worksheet.write(row_num, column, field_value, field_format)
+                    column += 1
+            elif isinstance(row, ExcelRow):
+                for cell in row.cells:
+                    cell.set_row(row_num)
+                    cell.set_first_data_row_num(first_data_row_num)
+                    worksheet.write(row_num, cell.column, cell.get_value(),
+                                    formats[cell.get_format_type()] if cell.get_format_type() in formats else None)
+
+            row_num += 1
+
+        workbook.close()
+
+        return output.getvalue()
