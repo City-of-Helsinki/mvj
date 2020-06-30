1b6907166919baf1de48b76091675612e3be9b33
Tomi Järvi <tomi@jarvi.pro>
2020-06-30 12:43:19 +0300

    Add tests to laske export

--- laske_export/tests/conftest.py
+++ laske_export/tests/conftest.py
@@ -1,1 +1,13 @@
+import tempfile
+
+from django.conf import settings
+
 from leasing.tests.conftest import *  # noqa
+
+
+def pytest_configure():
+    laske_export_root = tempfile.mkdtemp(prefix="laske-export-")
+    settings.LASKE_EXPORT_ROOT = laske_export_root
+    settings.LASKE_SERVERS = {
+        "export": {"host": "localhost", "username": "test", "password": "test"}
+    }
