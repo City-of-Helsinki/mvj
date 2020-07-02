79cea2ec9a1bdb4f6d1a5f6fca38e44be4bbf336
Tomi Järvi <tomi@jarvi.pro>
2020-07-02 14:37:31 +0300

    Add test for create invoice before tenant is active
    The users need to be able to create invoices for tenants who are not yet
    active.

--- leasing/tests/api/test_create_invoice.py
+++ leasing/tests/api/test_create_invoice.py
@@ -68,6 +68,63 @@ def test_create_invoice(
 
 
 @pytest.mark.django_db
+def test_create_invoice_before_tenant_contract_is_activated(
+    django_db_setup,
+    admin_client,
+    lease_factory,
+    tenant_factory,
+    tenant_rent_share_factory,
+    contact_factory,
+    tenant_contact_factory,
+):
+    lease = lease_factory(
+        type_id=1,
+        municipality_id=1,
+        district_id=1,
+        notice_period_id=1,
+        start_date=datetime.date(year=2000, month=1, day=1),
+        is_invoicing_enabled=True,
+    )
+
+    tenant1 = tenant_factory(lease=lease, share_numerator=1, share_denominator=1)
+    tenant_rent_share_factory(
+        tenant=tenant1, intended_use_id=1, share_numerator=1, share_denominator=1
+    )
+    contact1 = contact_factory(
+        first_name="First name 1", last_name="Last name 1", type=ContactType.PERSON
+    )
+
+    tenant_contact_factory(
+        type=TenantContactType.TENANT,
+        tenant=tenant1,
+        contact=contact1,
+        start_date=datetime.date.today() + datetime.timedelta(days=30),
+    )
+
+    data = {
+        "lease": lease.id,
+        "recipient": contact1.id,
+        "due_date": datetime.date.today() + datetime.timedelta(days=2),
+        "rows": [{"amount": Decimal(10), "receivable_type": 1}],
+    }
+
+    url = reverse("invoice-list")
+    response = admin_client.post(
+        url,
+        data=json.dumps(data, cls=DjangoJSONEncoder),
+        content_type="application/json",
+    )
+
+    assert response.status_code == 201, "%s %s" % (response.status_code, response.data)
+
+    invoice = Invoice.objects.get(pk=response.data["id"])
+
+    assert invoice.invoicing_date == timezone.now().date()
+    assert invoice.outstanding_amount == Decimal(10)
+    assert invoice.state == InvoiceState.OPEN
+
+
+@pytest.mark.django_db
 def test_create_zero_sum_invoice_state_is_paid(
     django_db_setup,
     admin_client,
