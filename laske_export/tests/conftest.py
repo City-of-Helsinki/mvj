59978b685d5a90b7d1e4576858d279918230cb01
Tomi Järvi <tomi@jarvi.pro>
2020-10-10 15:13:38 +0300

    Fix local testing of Laske Export
    Overwrite the default language setting to english.

--- laske_export/tests/conftest.py
+++ laske_export/tests/conftest.py
@@ -8,6 +8,7 @@ from leasing.tests.conftest import *  # noqa
 
 def pytest_configure():
     laske_export_root = tempfile.mkdtemp(prefix="laske-export-")
+    settings.LANGUAGE_CODE = "en"
     settings.LASKE_EXPORT_ROOT = laske_export_root
     settings.LASKE_SERVERS = {
         "export": {"host": "localhost", "username": "test", "password": "test"}
