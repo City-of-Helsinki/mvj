fe54060f7f879d175a3b52f8c7d50446caacb81f
Tomi Järvi <tomi@jarvi.pro>
2020-10-28 09:50:22 +0200

    Set user group permissions to plot search

--- leasing/management/commands/set_group_model_permissions.py
+++ leasing/management/commands/set_group_model_permissions.py
@@ -735,6 +735,15 @@ DEFAULT_MODEL_PERMS = {
         6: ("view",),
         7: ("view", "add", "change", "delete"),
     },
+    "plotsearch": {
+        1: None,
+        2: ("view", "add", "change", "delete"),
+        3: ("view",),
+        4: ("view", "add", "change", "delete"),
+        5: ("view", "add", "change", "delete"),
+        6: ("view",),
+        7: ("view", "add", "change", "delete"),
+    },
     "planunittype": {
         1: ("view",),
         2: ("view",),
