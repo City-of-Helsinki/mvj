c1432d82debcf9600bc1ace8655b839124d68f0e
Tomi Järvi <tomi@jarvi.pro>
2020-11-17 07:48:39 +0000

    Add test groups create command

---
+++ leasing/management/commands/create_test_user_groups.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import Group
+from django.core.management.base import BaseCommand
+
+GROUPS = {
+    1: "Selailija test",
+    2: "Valmistelija test",
+    3: "Sopimusvalmistelija test",
+    4: "Syöttäjä test",
+    5: "Perintälakimies test",
+    6: "Laskuttaja test",
+    7: "Pääkäyttäjä test",
+}
+
+
+class Command(BaseCommand):
+    """
+    Create test user groups for test purposes. The group permission updates not affect these groups.
+    """
+
+    def handle(self, *args, **options):
+        for group in Group.objects.filter(id__in=GROUPS.keys()):
+            (new_group, created) = Group.objects.get_or_create(
+                id=group.id + 10, defaults={"name": GROUPS[group.id]}
+            )
+            new_group.permissions.set(group.permissions.all())
