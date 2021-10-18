99b85ea93b410bf3924173512685ff370b2bbb2a
Niko Vuorinen <niko.vuorinen@vincit.fi>
2021-10-18 13:29:17 +0300

    Give read permissions to authenticated users to plotsearchsubtype endpoint

--- plotsearch/views.py
+++ plotsearch/views.py
@@ -1,5 +1,8 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import mixins
 from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 from rest_framework_gis.filters import InBBoxFilter
 
 from field_permissions.viewsets import FieldPermissionsViewsetMixin
@@ -21,9 +24,12 @@ from plotsearch.serializers import (
 )
 
 
-class PlotSearchSubtypeViewSet(AtomicTransactionModelViewSet):
+class PlotSearchSubtypeViewSet(
+    mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericViewSet
+):
     queryset = PlotSearchSubtype.objects.all()
     serializer_class = PlotSearchSubtypeSerializer
+    permission_classes = (IsAuthenticated,)
     filter_backends = (DjangoFilterBackend, OrderingFilter)
     filterset_fields = ["plot_search_type"]
 
