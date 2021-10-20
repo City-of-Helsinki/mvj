f3c508218121985e774dbc0ec1ca82f01cdcc04f
Niko Vuorinen <niko.vuorinen@vincit.fi>
2021-10-20 15:46:51 +0300

    Fix plotsearch test initializations

---
+++ plotsearch/tests/conftest.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+import pytest
+from django.core.management import call_command
+
+
+@pytest.fixture(scope="session")
+def django_db_setup(django_db_setup, django_db_blocker):
+    """Loads all the database fixtures in the leasing/fixtures folder"""
+    fixture_path = Path(__file__).parents[1] / "fixtures"
+    fixture_filenames = [path for path in fixture_path.glob("*") if not path.is_dir()]
+
+    with django_db_blocker.unblock():
+        call_command("loaddata", *fixture_filenames)
+
+    fixture_path = Path(__file__).parents[1].parent / "leasing/fixtures"
+    fixture_filenames = [path for path in fixture_path.glob("*") if not path.is_dir()]
+
+    with django_db_blocker.unblock():
+        call_command("loaddata", *fixture_filenames)
