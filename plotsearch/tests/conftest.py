ac073744f2051defd43e60d8d0cdd39d8ede87cd
Niko Vuorinen <niko.vuorinen@vincit.fi>
2021-10-20 15:51:56 +0300

    Fix comment

--- plotsearch/tests/conftest.py
+++ plotsearch/tests/conftest.py
@@ -6,7 +6,7 @@ from django.core.management import call_command
 
 @pytest.fixture(scope="session")
 def django_db_setup(django_db_setup, django_db_blocker):
-    """Loads all the database fixtures in the leasing/fixtures folder"""
+    """Loads all the database fixtures in the plotsearch/fixtures and leasing/fixtures folder"""
     fixture_path = Path(__file__).parents[1] / "fixtures"
     fixture_filenames = [path for path in fixture_path.glob("*") if not path.is_dir()]
 
