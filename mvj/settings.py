ab662cbe721376e57782f6ba4d319d0a56ddf41f
Niko Vuorinen <niko.vuorinen@vincit.fi>
2022-01-05 16:17:57 +0200

    Add localemiddleware to settings

--- mvj/settings.py
+++ mvj/settings.py
@@ -170,6 +170,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
