1de1af8ba7eb9bbb8ebd8d59fd4253a9f0c366f3
Niko Vuorinen <niko.vuorinen@vincit.fi>
2022-04-04 15:37:32 +0300

    Open API for every auth user, showing only authenticated info

---
+++ users/tests/api/test_user.py
@@ -0,0 +1,17 @@
+import pytest
+from rest_framework.reverse import reverse
+
+
+@pytest.mark.django_db
+def test_user_list(user_factory, client, admin_client):
+    user1 = user_factory()
+    user_factory()
+
+    client.force_login(user1)
+    url = reverse("user-list")
+    response = client.get(url)
+    assert len(response.json()["results"]) == 1
+    assert response.json()["results"][0]["username"] == user1.username
+
+    response = admin_client.get(url)
+    assert len(response.json()["results"]) == 3
--- users/viewsets.py
+++ users/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework import filters, viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from .models import User
 from .serializers import UserSerializer
@@ -9,3 +10,10 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = UserSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ("username", "first_name", "last_name")
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if not self.request.user.has_perm("users.view_user"):
+            qs = qs.filter(pk=self.request.user.pk)
+        return qs
