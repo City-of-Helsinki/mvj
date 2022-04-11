1b27f2d34688a84582e6642cddfc38ced946fbf8
Mikko Keskinen <keso@iki.fi>
2022-04-11 15:21:15 +0300

    Add ServiceUnit relation to users

--- users/models.py
+++ users/models.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 from helusers.models import AbstractUser
 from rest_framework.authtoken.models import Token
 
 
 class User(AbstractUser):
+    service_units = models.ManyToManyField("leasing.ServiceUnit", related_name="users")
+
     recursive_get_related_skip_relations = [
         "auth_token",
         "logentry",
@@ -14,6 +17,7 @@ class User(AbstractUser):
         "groups",
         "user_permissions",
         "ad_groups",
+        "service_units",
     ]
 
     @admin.display(boolean=True, description=_("Token exists"))
--- users/views.py
+++ users/views.py
@@ -5,6 +5,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from leasing.serializers.service_unit import ServiceUnitSerializer
+
 
 class UsersPermissions(APIView):
     permission_classes = (IsAuthenticated,)
@@ -12,7 +14,7 @@ class UsersPermissions(APIView):
     def get_view_name(self):
         return _("Users permissions")
 
-    def get(self, request, format=None, contract_id=None, file_id=None):
+    def get(self, request, format=None):
         if request.user.is_superuser:
             permissions = Permission.objects.all()
         else:
@@ -23,10 +25,13 @@ class UsersPermissions(APIView):
                     permissions.update(backend._get_group_permissions(request.user))
 
         groups = [g.name for g in request.user.groups.all()]
+        service_units = request.user.service_units.all()
+        service_units_serializer = ServiceUnitSerializer(service_units, many=True)
 
         return Response(
             {
                 "groups": groups,
+                "service_units": service_units_serializer.data,
                 "permissions": [
                     {"name": p.name, "codename": p.codename} for p in permissions
                 ],
