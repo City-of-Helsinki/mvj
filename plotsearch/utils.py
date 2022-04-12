161088e8366d6d53065468270d1efa4541446393
Niko Vuorinen <niko.vuorinen@vincit.fi>
2022-04-12 15:49:49 +0300

    Create listview for showing duplicated sections

---
+++ plotsearch/utils.py
@@ -0,0 +1,15 @@
+def merge_dicts(a, b, path=None):
+    "merges b into a"
+    if path is None:
+        path = []
+    for key in b:
+        if key in a:
+            if isinstance(a[key], dict) and isinstance(b[key], dict):
+                merge_dicts(a[key], b[key], path + [str(key)])
+            elif a[key] == b[key]:
+                pass  # same leaf value
+            else:
+                raise Exception("Conflict at %s" % ".".join(path + [str(key)]))
+        else:
+            a[key] = b[key]
+    return a
