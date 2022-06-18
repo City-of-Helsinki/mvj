81aa71123cb1673625a8e4dfd499ead7a4244a27
Mikko Keskinen <keso@iki.fi>
2022-06-18 15:26:44 +0300

    Add a couple of test to the report view

--- leasing/tests/report/test_reports.py
+++ leasing/tests/report/test_reports.py
@@ -1,13 +1,34 @@
 from multiprocessing import Event, Value
 
 import pytest
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 from django.core import mail
+from django.urls import reverse
 from django_q.brokers import get_broker
 from django_q.cluster import monitor, pusher, worker
 from django_q.queues import Queue
 from django_q.tasks import queue_size
 
 from leasing.report.lease.lease_statistic_report import LeaseStatisticReport
+from leasing.report.viewset import ENABLED_REPORTS
+
+
+def _add_report_permission(user, reports):
+    report_content_type, created = ContentType.objects.get_or_create(
+        app_label="leasing", model="report"
+    )
+
+    if not isinstance(reports, list):
+        reports = [reports]
+
+    for report in reports:
+        codename = "can_generate_report_{}".format(report.slug)
+        permission, created = Permission.objects.get_or_create(
+            name=codename, codename=codename, content_type=report_content_type,
+        )
+
+        user.user_permissions.add(permission)
 
 
 @pytest.fixture(autouse=True)
@@ -55,3 +76,53 @@ def test_simple_async_report_send(rf, admin_user):
     # Test report file have been sent via email
     assert len(mail.outbox) == 1
     assert len(mail.outbox[0].attachments) == 1
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("report", ENABLED_REPORTS)
+def test_report_options_respond_with_ok(admin_client, report):
+    url = reverse("report-detail", kwargs={"report_type": report.slug})
+
+    response = admin_client.options(url)
+
+    assert response.status_code == 200
+    assert response.json() is not None
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("report", ENABLED_REPORTS)
+def test_can_generate_report_permission(client, user, report):
+    client.force_login(user)
+
+    url = reverse("report-detail", kwargs={"report_type": report.slug})
+
+    response = client.get(url)
+    assert response.status_code == 403
+
+    _add_report_permission(user, report)
+
+    response = client.get(url)
+    assert response.status_code == 200 or response.status_code == 400
+
+
+@pytest.mark.django_db
+def test_report_list(client, user):
+    client.force_login(user)
+
+    url = reverse("report-list")
+
+    response = client.get(url)
+    data = response.json()
+
+    assert response.status_code == 200
+    assert len(data) == 0
+
+    _add_report_permission(user, ENABLED_REPORTS[-3:])
+
+    response = client.get(url)
+    data = response.json()
+
+    assert response.status_code == 200
+    assert len(data) == 3
+
+    assert {r.slug for r in ENABLED_REPORTS[-3:]} == set(data.keys())
