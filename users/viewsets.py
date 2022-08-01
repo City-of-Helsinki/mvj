8d28f82802700233d5f237a73281d527dbb58319
Mikko Keskinen <keso@iki.fi>
2022-08-01 08:54:16 +0300

    Add ability to filter Users by Service Unit

--- users/viewsets.py
+++ users/viewsets.py
@@ -1,6 +1,8 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, viewsets
 from rest_framework.permissions import IsAuthenticated
 
+from .filters import UserFilter
 from .models import User
 from .serializers import UserSerializer
 
@@ -8,12 +10,15 @@ from .serializers import UserSerializer
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    filter_backends = (filters.SearchFilter,)
+    filterset_class = UserFilter
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     search_fields = ("username", "first_name", "last_name")
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         qs = super().get_queryset()
+
         if not self.request.user.has_perm("users.view_user"):
             qs = qs.filter(pk=self.request.user.pk)
-        return qs
+
+        return qs.distinct()
