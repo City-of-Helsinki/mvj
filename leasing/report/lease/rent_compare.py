f24047660f1fb44be46eae163c35b3c41b414af8
Mikko Keskinen <keso@iki.fi>
2022-11-28 14:52:02 +0200

    Changes to rent_compare report
    - Skip leases that have no rents
    - Skip one-time rents
    - Add start_date, end_date, and rent_type output fields

--- leasing/report/lease/rent_compare.py
+++ leasing/report/lease/rent_compare.py
@@ -5,6 +5,7 @@ from django import forms
 from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
 
+from leasing.enums import RentType
 from leasing.models import Lease
 from leasing.report.report_base import AsyncReportBase
 
@@ -23,8 +24,11 @@ class RentCompareReport(AsyncReportBase):
     }
     output_fields = {
         "lease_id": {"label": _("Lease identifier"), "width": 13},
-        "first_year": {"label": _("First year rent"), "format": "money", "width": 13},
-        "second_year": {"label": _("Second year rent"), "format": "money", "width": 13},
+        "start_date": {"label": "Start date", "format": "date"},
+        "end_date": {"label": "End date", "format": "date"},
+        "rent_type": {"label": _("Rent type")},
+        "first_year": {"label": _("First year rent"), "format": "money", "width": 20},
+        "second_year": {"label": _("Second year rent"), "format": "money", "width": 20},
         "difference": {"label": _("Difference percent"), "format": "percentage"},
     }
     async_task_timeout = 60 * 30  # 30 minutes
@@ -83,22 +87,31 @@ class RentCompareReport(AsyncReportBase):
                     )
                 )
             )
+            .exclude(rents__deleted__isnull=False)
+            .exclude(rents__type=RentType.ONE_TIME)
             .order_by(
                 "identifier__type__identifier",
                 "identifier__municipality__identifier",
                 "identifier__district__identifier",
                 "identifier__sequence",
             )
+            .distinct()
         )
 
         results = []
         for lease in leases:
             result = {
                 "lease_id": lease.get_identifier_string(),
+                "start_date": lease.start_date,
+                "end_date": lease.end_date,
+                "rent_type": None,
                 "first_year": None,
                 "second_year": None,
                 "difference": None,
             }
+
+            rent_types = {str(rent.type) for rent in lease.rents.all()}
+            result["rent_type"] = ", ".join(rent_types)
 
             for year in [first_year, second_year]:
                 year_key = "first_year" if year == first_year else "second_year"
@@ -109,6 +122,9 @@ class RentCompareReport(AsyncReportBase):
                 except NotImplementedError:
                     # Ignore the rent if the rent doesn't have an index defined
                     pass
+
+            if not result["first_year"] and not result["second_year"]:
+                continue
 
             if result["first_year"] and result["second_year"]:
                 difference = abs(result["second_year"] - result["first_year"])
