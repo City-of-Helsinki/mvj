36b4a6d19e6254d24676ebfb891fd4a0b8867a76
Niko Vuorinen <niko.vuorinen@vincit.fi>
2023-03-23 15:03:18 +0200

    Fix Usage Importer and add it to mvj_import

--- leasing/importer/usage_distributions.py
+++ leasing/importer/usage_distributions.py
@@ -9,6 +9,10 @@ from leasing.models.land_area import UsageDistribution
 class UsageDistributionImporter(BaseImporter):
     type_name = "usage_distributions"
 
+    def __init__(self, stdout=None, stderr=None):
+        self.stdout = stdout
+        self.stderr = stderr
+
     def initialize_importer(self):
         import oracledb
 
@@ -44,32 +48,38 @@ class UsageDistributionImporter(BaseImporter):
         self.initialize_importer()
         cursor = self.cursor
 
+        # fmt: off
         query = """
-            SELECT A.kg_kkaavyks , A.c_kaavayksikkotunnus , A.c_kaytjakauma , mv_koodisto0.c_selite AS mv_koodisto0_c_selite , A.c_paasivuk , A.c_rakoikeus  # noqa: E501
+            SELECT A.kg_kkaavyks , A.c_kaavayksikkotunnus , A.c_kaytjakauma , mv_koodisto0.c_selite AS mv_koodisto0_c_selite , A.c_paasivuk , A.c_rakoikeus
             FROM MV_KAAVAYKSIKON_RAKOIKJAKAUMA A
-            LEFT OUTER JOIN (select c_koodi, decode('FIN', 'FIN', decode(trim(c_selite), null, c_seliter, c_selite), decode(trim(c_seliter), null, c_selite, c_seliter)) AS c_selite FROM mv_koodisto  # noqa: E501
+            LEFT OUTER JOIN (select c_koodi, decode('FIN', 'FIN', decode(trim(c_selite), null, c_seliter, c_selite), decode(trim(c_seliter), null, c_selite, c_seliter)) AS c_selite FROM mv_koodisto
             WHERE c_koodisto='SU_KAYTJAKAUMA') mv_koodisto0 ON (A.c_kaytjakauma=mv_koodisto0.c_koodi)
             ORDER BY A.c_kaavayksikkotunnus ASC
             """
+        # fmt: on
         cursor.execute(query)
 
         usage_distribution_rows = rows_to_dict_list(cursor)
         pivot_plan_unit = None
 
         for usage_distribution_row in usage_distribution_rows:
-            try:
-                plan_unit = PlanUnit.objects.get(
-                    identifier=usage_distribution_row["C_KAAVAYKSIKKOTUNNUS"]
-                )
-            except PlanUnit.DoesNotExist:
-                continue
-
-            if plan_unit != pivot_plan_unit:
-                plan_unit.usage_distributions.all().delete()
-                pivot_plan_unit = plan_unit
-
-            UsageDistribution.objects.create(
-                plan_unit=plan_unit,
-                distribution=usage_distribution_row["C_KAYTJAKAUMA"],
-                build_permission=usage_distribution_row["C_RAKOIKEUS"],
+            plan_unit_id = "-".join(
+                str(int(num))
+                for num in usage_distribution_row["C_KAAVAYKSIKKOTUNNUS"].split("-")
             )
+
+            plan_unit_qs = PlanUnit.objects.filter(identifier=plan_unit_id)
+            if pivot_plan_unit is None:
+                pivot_plan_unit = plan_unit_qs.first()
+                pivot_plan_unit.usage_distributions.all().delete()
+
+            for plan_unit in plan_unit_qs:
+                if plan_unit.id != pivot_plan_unit.id:
+                    plan_unit.usage_distributions.all().delete()
+                    pivot_plan_unit = plan_unit
+
+                UsageDistribution.objects.create(
+                    plan_unit=plan_unit,
+                    distribution=usage_distribution_row["C_KAYTJAKAUMA"] or "-",
+                    build_permission=usage_distribution_row["C_RAKOIKEUS"] or "-",
+                )
