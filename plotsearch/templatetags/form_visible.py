6706f27bb79d694d64ef2e5564b3326eb921f0c0
Niko Vuorinen <niko.vuorinen@vincit.fi>
2023-06-30 14:46:11 +0300

    Fix Choices

--- plotsearch/templatetags/form_visible.py
+++ plotsearch/templatetags/form_visible.py
@@ -1,5 +1,6 @@
 from django import template
 
+from forms.models import Choice
 
 register = template.Library()
 
@@ -11,3 +12,11 @@ def visible_section(section, value):
 @register.filter
 def visible_field(field, value):
     return field.filter(enabled=value)
+
+@register.simple_tag
+def fetch_choice_value(value):
+    try:
+        choice = Choice.objects.get(id=value).value
+    except ValueError:
+        choice = "-"
+    return choice
