f4a31e3a8c1fac3702a8cd28c5b737515cea9fd1
Jukka Ahonen <jukka.ahonen@nordcloud.com>
2023-09-29 13:07:13 +0300

    open invoices report: filter by service unit

--- leasing/report/invoice/open_invoices_report.py
+++ leasing/report/invoice/open_invoices_report.py
@@ -9,6 +9,7 @@ from leasing.enums import InvoiceState
 from leasing.models import Invoice
 from leasing.report.excel import ExcelCell, ExcelRow, PreviousRowsSumCell, SumCell
 from leasing.report.report_base import ReportBase
+from leasing.enums import ServiceUnit
 
 
 def get_lease_type(obj):
@@ -36,6 +37,9 @@ class OpenInvoicesReport(ReportBase):
     description = _('Show all the invoices that have their state as "open"')
     slug = "open_invoices"
     input_fields = {
+        "service_unit": forms.ChoiceField(
+            label=_("Palvelukokonaisuus"), required=False, choices=ServiceUnit.choices()
+        ),
         "start_date": forms.DateField(label=_("Start date"), required=True),
         "end_date": forms.DateField(label=_("End date"), required=True),
     }
@@ -64,7 +68,7 @@ class OpenInvoicesReport(ReportBase):
     }
 
     def get_data(self, input_data):
-        return (
+        qs = (
             Invoice.objects.filter(
                 due_date__gte=input_data["start_date"],
                 due_date__lte=input_data["end_date"],
@@ -80,6 +84,11 @@ class OpenInvoicesReport(ReportBase):
             )
             .order_by("lease__identifier__type__identifier", "due_date")
         )
+
+        if input_data["service_unit"]:
+            qs = qs.filter(service_unit=input_data["service_unit"])
+
+        return qs
 
     def get_response(self, request):
         report_data = self.get_data(self.get_input_data(request))
