f7dbe62451f0fd44cf878107a3202593c575fdd9
esa valimaki <esa.valimaki@nordcloud.com>
2024-03-12 18:12:16 +0200

    Add test for finding unauthenticated endpoints, mainly from officer ui
    Relates to MVJ-102

---
+++ utils/tests/test_unauthenticated_endpoints.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import pytest
+from django_extensions.management.commands import show_urls
+import json
+import requests
+from lxml import html
+import mvj.urls
+
+
+def paths():
+    cmd = show_urls.Command()
+    views_str = cmd.handle(
+        no_color=True,
+        language=None, decorator=None,
+        format_style='pretty-json',
+        urlconf="ROOT_URLCONF",unsorted=True)
+    views = json.loads(views_str)
+    return [path for view in views if (path := view.get('url'))]
+
+repls = (('/<pk>\.<format>/','/1234.json/'),
+         ('/<pk>/','/1234/'),
+         ('/<path:object_id>/','/abc/123/'))
+
+loginpagetitle = 'Kirjaudu sisään | Django-sivuston ylläpito'
+
+whitelist = '''
+    /auth/login/
+    /auth/logout/
+    /v1/pub/answer/
+    /v1/pub/faq/
+    /v1/pub/form/
+    /v1/pub/plot_search_ui/0
+    /v1/pub/plot_search/
+    /v1/pub/plot_search_stage/
+    /v1/pub/plot_search_type/
+    /v1/pub/plot_search_subtype/
+    /v1/pub/plot_search_ui/1
+    /docs/
+    /redoc/
+''' .split()
+
+@pytest.mark.parametrize('path',paths())
+def test_urls(path):
+    if not path.endswith('/') or path in whitelist: pytest.skip()
+    for r in repls:
+        path = path.replace(*r)
+    if '>' in path: pytest.skip() # This includes lone '\.<format>' and few corner cases
+    url = f'http://localhost:8001{path}'
+    res = requests.get(url)
+    ct = res.headers.get('content-type')
+    sc = res.status_code
+    if sc == 200 :
+        assert ct.startswith('text/html;'), f"Got {ct} response # curl -L {url}"
+        # We are viewing internal pages so trusting the title should be adequate
+        titles = html.fromstring(res.content).xpath('//title/text()')
+        assert titles[0].encode('latin1').decode('utf-8') == loginpagetitle, f"Got non-login {ct} response # curl -L {url}"
+    else:
+        assert res.status_code == 401 or res.status_code == 404 , f"Got unexpected retcode {res.status_code} # curl -L -v {url}"
