66f8b7174251ec749b81613a302e423bbb66b8d4
esa valimaki <esa.valimaki@nordcloud.com>
2024-03-19 14:28:50 +0200

    black format

--- utils/tests/test_unauthenticated_endpoints.py
+++ utils/tests/test_unauthenticated_endpoints.py
@@ -11,20 +11,25 @@ def get_url_paths():
     cmd = show_urls.Command()
     views_str = cmd.handle(
         no_color=True,
-        language=None, decorator=None,
-        format_style='pretty-json',
-        urlconf="ROOT_URLCONF",unsorted=True)
+        language=None,
+        decorator=None,
+        format_style="pretty-json",
+        urlconf="ROOT_URLCONF",
+        unsorted=True,
+    )
     views = json.loads(views_str)
-    return [path for view in views if (path := view.get('url'))]
+    return [path for view in views if (path := view.get("url"))]
 
 
-@pytest.mark.parametrize('path',get_url_paths())
+@pytest.mark.parametrize("path", get_url_paths())
 def test_urls(path):
-    repls = (('/<pk>\.<format>/','/1234.json/'),
-         ('/<pk>/','/1234/'),
-         ('/<path:object_id>/','/abc/123/'))
-    loginpagetitle = 'Kirjaudu sisään | Django-sivuston ylläpito'
-    whitelist = '''
+    repls = (
+        ("/<pk>\.<format>/", "/1234.json/"),
+        ("/<pk>/", "/1234/"),
+        ("/<path:object_id>/", "/abc/123/"),
+    )
+    loginpagetitle = "Kirjaudu sisään | Django-sivuston ylläpito"
+    whitelist = """
         /auth/login/
         /auth/logout/
         /v1/pub/answer/
@@ -38,20 +43,26 @@ def test_urls(path):
         /v1/pub/plot_search_ui/1
         /docs/
         /redoc/
-    ''' .split()
-    if not path.endswith('/') or path in whitelist: pytest.skip()
+    """.split()
+    if not path.endswith("/") or path in whitelist:
+        pytest.skip()
     for r in repls:
         path = path.replace(*r)
-    if '>' in path: pytest.skip() # This includes lone '\.<format>' and few corner cases
-    url = f'http://localhost:8001{path}'
-    response = requests.get(url, headers = {'Content-Type': 'application/json'})
-    content_type = response.headers.get('content-type')
-    if response.status_code == 200 :
-        assert content_type.startswith('text/html;'), f"Got {content_type} response # curl -L {url}"
+    if ">" in path:
+        pytest.skip()  # This includes lone '\.<format>' and few corner cases
+    url = f"http://localhost:8001{path}"
+    response = requests.get(url, headers={"Content-Type": "application/json"})
+    content_type = response.headers.get("content-type")
+    if response.status_code == 200:
+        assert content_type.startswith(
+            "text/html;"
+        ), f"Got {content_type} response # curl -L {url}"
         # We are viewing internal pages so trusting the title should be adequate
-        titles = html.fromstring(response.content).xpath('//title/text()')
-        assert titles[0].encode('latin1').decode('utf-8') == loginpagetitle,\
-               f"Got non-login {content_type} response # curl -L {url}"
+        titles = html.fromstring(response.content).xpath("//title/text()")
+        assert (
+            titles[0].encode("latin1").decode("utf-8") == loginpagetitle
+        ), f"Got non-login {content_type} response # curl -L {url}"
     else:
-        assert response.status_code == 401 or response.status_code == 404,\
-               f"Got unexpected retcode {response.status_code} # curl -L -v {url}"
+        assert (
+            response.status_code == 401 or response.status_code == 404
+        ), f"Got unexpected retcode {response.status_code} # curl -L -v {url}"
