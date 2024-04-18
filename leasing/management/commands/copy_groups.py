7f25ed41e8edf3334489e6d687b0dbe6a4e890dc
Henri Nieminen <henri.nieminen@nordcloud.com>
2024-04-18 12:00:44 +0000

    Add service units to copy groups script
    Refactor by removing reliance on id's, rely on group names.
    Ensure group id's should not collide by incrementing them more than by 10,
    script already hit the limit with more than 10 groups.

--- leasing/management/commands/copy_groups.py
+++ leasing/management/commands/copy_groups.py
@@ -1,21 +1,38 @@
 from django.contrib.auth.models import Group
 from django.core.management.base import BaseCommand
 
-GROUPS = {
-    1: "Selailija test",
-    2: "Valmistelija test",
-    3: "Sopimusvalmistelija test",
-    4: "Syöttäjä test",
-    5: "Perintälakimies test",
-    6: "Laskuttaja test",
-    7: "Pääkäyttäjä test",
-}
+GROUPS = [
+    # User roles
+    "Selailija",
+    "Valmistelija",
+    "Sopimusvalmistelija",
+    "Syöttäjä",
+    "Perintälakimies",
+    "Laskuttaja",
+    "Pääkäyttäjä",
+    # Service units
+    "Maaomaisuuden kehittäminen ja tontit",
+    "Alueiden käyttö ja valvonta",
+    "KuVa / Liikuntapaikkapalvelut",
+    "KuVa / Ulkoilupalvelut",
+    "KuVa / Nuorisopalvelut",
+]
 
 
 class Command(BaseCommand):
+    """
+    Makes copies of GROUPS with the same permissions.
+    The test groups are named "TEST <group name>".
+    Since groups are synced from AD groups and flushed periodically,
+    this is a way to make permissions stick for e.g. devs.
+    """
+
     def handle(self, *args, **options):
-        for group in Group.objects.filter(id__in=GROUPS.keys()):
-            (new_group, created) = Group.objects.get_or_create(
-                id=group.id + 10, defaults={"name": GROUPS[group.id]}
+        groups = Group.objects.filter(name__in=GROUPS)
+        for group in groups:
+            test_group_name = f"TEST {group.name}"
+            (test_group, created) = Group.objects.get_or_create(
+                name=test_group_name,
+                defaults={"id": group.id + 1000, "name": test_group_name},
             )
-            new_group.permissions.set(group.permissions.all())
+            test_group.permissions.set(group.permissions.all())
