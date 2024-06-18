d134287b0cb316515fade3edf52d0631ba570015
Henri Nieminen <118905702+henrinie-nc@users.noreply.github.com>
2024-06-18 10:33:19 +0300

    Mvj 329 fix invoice count report (#659)
    * fix and refactor invoice count report
    
    * fix app_makemessages & update translations
    
    * add test and adjust typing for laske invoice count report

--- leasing/tests/report/test_reports.py
+++ leasing/tests/report/test_reports.py
@@ -1,15 +1,20 @@
+from datetime import datetime
 from multiprocessing import Event, Value
+from unittest.mock import patch
 
 import pytest
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 from django.core import mail
 from django.urls import reverse
+from django.utils.timezone import make_aware, now
 from django_q.brokers import get_broker
 from django_q.cluster import monitor, pusher, worker
 from django_q.queues import Queue
 from django_q.tasks import queue_size
 
+from leasing.enums import DueDatesType, InvoiceState
+from leasing.report.invoice.laske_invoice_count_report import LaskeInvoiceCountReport
 from leasing.report.lease.lease_statistic_report import LeaseStatisticReport
 from leasing.report.viewset import ENABLED_REPORTS
 
@@ -128,3 +133,98 @@ def test_report_list(client, user):
     assert len(data) == 3
 
     assert {r.slug for r in ENABLED_REPORTS[-3:]} == set(data.keys())
+
+
+@pytest.mark.django_db
+def test_laske_invoice_count_report(
+    client,
+    user,
+    invoice_factory,
+    service_unit_factory,
+    lease_factory,
+    contact_factory,
+    rent_factory,
+):
+    _add_report_permission(user, LaskeInvoiceCountReport)
+    client.force_login(user)
+
+    service_unit = service_unit_factory()
+    lease = lease_factory(
+        service_unit=service_unit, is_invoicing_enabled=True, end_date="2030-12-31"
+    )
+    contact = contact_factory()
+    dates = [
+        make_aware(datetime(2024, 6, 13)),
+        make_aware(datetime(2024, 6, 14)),
+        make_aware(datetime(2024, 6, 15)),
+    ]
+    for date in dates:
+        invoice_factory.create(
+            state=InvoiceState.OPEN,
+            sent_to_sap_at=date,
+            lease=lease,
+            service_unit=service_unit,
+            recipient=contact,
+            total_amount=1000,
+            billed_amount=1000,
+        )
+
+    # Create rent that should generate future invoices, to be listed in estimated_count
+    rent_factory(
+        lease=lease,
+        start_date=make_aware(datetime(2024, 1, 1)),
+        end_date=None,
+        due_dates_type=DueDatesType.FIXED,
+        due_dates_per_year=12,  # Monthly invoices, expected at the beginning of the month
+    )
+    # Create invoice in the future, to be listed in estimated_count
+    invoice_factory(
+        state=InvoiceState.OPEN,
+        sent_to_sap_at=None,
+        lease=lease,
+        service_unit=service_unit,
+        recipient=contact,
+        total_amount=1000,
+        billed_amount=1000,
+        due_date=make_aware(datetime(2024, 7, 16)),
+    )
+
+    with patch("leasing.report.invoice.laske_invoice_count_report.now") as mock_now:
+        # Freeze "today" to 2024-06-15 08:01:01 in django.utils.timezone.now() used in code
+        mock_now.return_value = now().replace(
+            year=2024, month=6, day=15, hour=8, minute=1, second=1
+        )
+
+        url = reverse(
+            "v1:report-detail",
+            kwargs={
+                "report_type": LaskeInvoiceCountReport.slug,
+            },
+        )
+        query_params = {
+            "start_date": "2024-06-13",
+            "end_date": "2024-08-18",
+            "service_unit": service_unit.id,
+        }
+        response = client.get(url, data=query_params)
+
+    assert response.status_code == 200
+
+    response_data_dict = {
+        item["send_date"].isoformat(): {
+            "invoice_count": item["invoice_count"],
+            "estimate_count": item["estimate_count"],
+        }
+        for item in response.data
+    }
+
+    assert response_data_dict == {
+        "2024-06-13": {"invoice_count": 1, "estimate_count": 0},
+        "2024-06-14": {"invoice_count": 1, "estimate_count": 0},
+        "2024-06-15": {"invoice_count": 1, "estimate_count": 0},
+        # Manually created invoice not yet sent to SAP
+        "2024-06-16": {"invoice_count": 0, "estimate_count": 1},
+        # Invoices coming from rent
+        "2024-07-01": {"invoice_count": 0, "estimate_count": 1},
+        "2024-08-01": {"invoice_count": 0, "estimate_count": 1},
+    }
