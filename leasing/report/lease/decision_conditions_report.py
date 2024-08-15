ea18075ab65df34764d424b3d2dfb8937c22ac8a
Robert Rytövuori <166599090+robertrytovuori@users.noreply.github.com>
2024-08-15 10:59:08 +0300

    Change decision conditions report supervision exists to ChoiceField (#691)
    * decision conditions report change supervision exists field to ChoiceField to accommodate null value
    
    * simplify decision conditions report supervision exists logic

--- leasing/report/lease/decision_conditions_report.py
+++ leasing/report/lease/decision_conditions_report.py
@@ -73,8 +73,10 @@ class DecisionConditionsReport(ReportBase):
             empty_label=None,
             required=False,
         ),
-        "supervision_exists": forms.NullBooleanField(
-            label=_("Supervision date exists"), initial=False, required=False
+        "supervision_exists": forms.ChoiceField(
+            choices=[("True", _("Yes")), ("False", _("No"))],
+            label=_("Supervision date exists"),
+            required=False,
         ),
     }
     output_fields = {
@@ -128,10 +130,11 @@ class DecisionConditionsReport(ReportBase):
         if input_data["service_unit"]:
             qs = qs.filter(decision__lease__service_unit__in=input_data["service_unit"])
 
-        if input_data["supervision_exists"] is not None:
-            qs = qs.filter(
-                supervision_date__isnull=not input_data["supervision_exists"]
-            )
+        if input_data["supervision_exists"]:
+            if input_data["supervision_exists"] == "True":
+                qs = qs.filter(supervision_date__isnull=False)
+            else:
+                qs = qs.filter(supervision_date__isnull=True)
 
         if input_data["start_date"]:
             qs = qs.filter(supervision_date__gte=input_data["start_date"])
