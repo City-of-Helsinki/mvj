f72d953dfad584535ad488870ab81e90db0de20f
Robert Rytövuori <166599090+robertrytovuori@users.noreply.github.com>
2024-08-22 14:04:00 +0300

    Mvj 416 invoicing review sections on own excel sheets (#704)
    * minor refactoring of data_as_excel function
    
    * refactoring data_as_excel function
    
    * invoicing review sections onto own excel sheets
    
    * write_dict_row_to_worksheet too complex flake error fix
    
    * writing documentation
    
    * remove extra trailing empty row from write_input_field_value_rows
    
    * add comment

--- leasing/report/report_base.py
+++ leasing/report/report_base.py
@@ -178,9 +178,7 @@ class ReportBase:
 
         return value
 
-    def data_as_excel(  # NOQA C901 'ReportBase.data_as_excel' is too complex
-        self, data
-    ):
+    def data_as_excel(self, data):
         report = self
 
         output = BytesIO()
@@ -209,35 +207,9 @@ class ReportBase:
 
         # On the fourth row forwards print the input fields and their values
         row_num += 2
-        for input_field_name, input_field in report.form.fields.items():
-            worksheet.write(
-                row_num, 0, "{}:".format(input_field.label), formats[FormatType.BOLD]
-            )
-            field_format = None
-            if input_field.__class__.__name__ == "DateField":
-                field_format = formats[FormatType.DATE]
-
-            input_value = report.form.cleaned_data[input_field_name]
-            if hasattr(input_field, "choices"):
-                for choice_value, choice_label in input_field.choices:
-                    if choice_value == input_value:
-                        input_value = str(choice_label)
-                        break
-
-            if isinstance(input_value, Model):
-                input_value = str(input_value)
-
-            if isinstance(input_value, bool):
-                if input_value:
-                    input_value = gettext("Yes")
-                else:
-                    input_value = gettext("No")
-
-            if isinstance(input_value, QuerySet):
-                input_value = ", ".join([str(v) for v in input_value.all()])
-
-            worksheet.write(row_num, 1, input_value, field_format)
-            row_num += 1
+        row_num = self.write_input_field_value_rows(
+            worksheet, report.form, row_num, formats
+        )
 
         # Set column widths
         for index, field_name in enumerate(report.output_fields.keys()):
@@ -274,43 +246,7 @@ class ReportBase:
         first_data_row_num = row_num
         for row in data:
             if isinstance(row, dict):
-                column = 0
-                for field_name, field_value in row.items():
-                    field_format = None
-
-                    field_format_name = report.get_output_field_attr(
-                        field_name, "format"
-                    )
-
-                    if field_format_name == "date":
-                        field_format = formats[FormatType.DATE]
-                    if field_format_name == "percentage":
-                        field_format = formats[FormatType.PERCENTAGE]
-                        if field_value:
-                            field_value /= 100
-                    elif field_format_name == "money":
-                        if field_value != 0:
-                            field_format = formats[FormatType.MONEY]
-                    elif field_format_name == "boolean":
-                        if field_value:
-                            field_value = str(_("Yes"))
-                        else:
-                            field_value = str(_("No"))
-                    elif field_format_name == "area":
-                        field_format = formats[FormatType.AREA]
-
-                    field_serializer_field = report.get_output_field_attr(
-                        field_name, "serializer_field"
-                    )
-                    if isinstance(field_serializer_field, ChoiceField):
-                        field_value = (
-                            str(field_serializer_field.choices.get(field_value))
-                            if field_value
-                            else ""
-                        )
-
-                    worksheet.write(row_num, column, field_value, field_format)
-                    column += 1
+                self.write_dict_row_to_worksheet(worksheet, formats, row_num, row)
             elif isinstance(row, ExcelRow):
                 for cell in row.cells:
                     cell.set_row(row_num)
@@ -331,6 +267,89 @@ class ReportBase:
         workbook.close()
 
         return output.getvalue()
+
+    def write_dict_row_to_worksheet(self, worksheet, formats, row_num, row):
+        """
+        Writes row on worksheet on given row number from dict with relevant formatting.
+        """
+        column = 0
+        for field_name, field_value in row.items():
+            field_format = None
+
+            field_format_name = self.get_output_field_attr(field_name, "format")
+
+            match field_format_name:
+                case "date":
+                    field_format = formats[FormatType.DATE]
+                case "percentage":
+                    field_format = formats[FormatType.PERCENTAGE]
+                    if field_value:
+                        field_value /= 100
+                case "money":
+                    if field_value != 0:
+                        field_format = formats[FormatType.MONEY]
+                case "boolean":
+                    if field_value:
+                        field_value = str(_("Yes"))
+                    else:
+                        field_value = str(_("No"))
+                case "area":
+                    field_format = formats[FormatType.AREA]
+
+            field_serializer_field = self.get_output_field_attr(
+                field_name, "serializer_field"
+            )
+            if isinstance(field_serializer_field, ChoiceField):
+                field_value = (
+                    str(field_serializer_field.choices.get(field_value))
+                    if field_value
+                    else ""
+                )
+
+            worksheet.write(row_num, column, field_value, field_format)
+            column += 1
+
+    def write_input_field_value_rows(
+        self, worksheet, report_form, row_num_cursor, formats
+    ):
+        """
+        Writes form input values from report form onto given worksheet at given row number. Requires formats as well.
+        Returns new row num at end of row data written by function.
+        """
+        for input_field_name, input_field in report_form.fields.items():
+            worksheet.write(
+                row_num_cursor,
+                0,
+                "{}:".format(input_field.label),
+                formats[FormatType.BOLD],
+            )
+            field_format = None
+            if input_field.__class__.__name__ == "DateField":
+                field_format = formats[FormatType.DATE]
+
+            input_value = report_form.cleaned_data[input_field_name]
+            if hasattr(input_field, "choices"):
+                for choice_value, choice_label in input_field.choices:
+                    if choice_value == input_value:
+                        input_value = str(choice_label)
+                        break
+
+            if isinstance(input_value, Model):
+                input_value = str(input_value)
+
+            if isinstance(input_value, bool):
+                if input_value:
+                    input_value = gettext("Yes")
+                else:
+                    input_value = gettext("No")
+
+            if isinstance(input_value, QuerySet):
+                input_value = ", ".join([str(v) for v in input_value.all()])
+
+            worksheet.write(row_num_cursor, 1, input_value, field_format)
+            row_num_cursor += 1
+
+        return row_num_cursor
 
 
 class AsyncReportBase(ReportBase):
