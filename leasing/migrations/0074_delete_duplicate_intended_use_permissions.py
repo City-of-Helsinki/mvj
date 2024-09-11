699d994bc9d569e7d5d7f6832ae11f05313b0e84
Jukka Ahonen <jukka.ahonen@nordcloud.com>
2024-09-11 11:28:58 +0000

    make migration more explicit

--- leasing/migrations/0074_delete_duplicate_intended_use_permissions.py
+++ leasing/migrations/0074_delete_duplicate_intended_use_permissions.py
@@ -7,10 +7,26 @@ def remove_duplicate_intended_use_permissions(apps, schema_editor):
     """
     Remove duplicate intended use permissions and the related auth group permissions.
     """
-    Permission = apps.get_model("auth", "Permission")
-    permissions = Permission.objects.filter(
-        name__contains="Area search intended use",
-        codename__contains="_intendeduse",
+    permission = apps.get_model("auth", "Permission")
+
+    permission.objects.filter(
+        name="Can view Area search intended use",
+        codename="view_intendeduse",
+    ).delete()
+
+    permission.objects.filter(
+        name="Can add Area search intended use",
+        codename="add_intendeduse",
+    ).delete()
+
+    permission.objects.filter(
+        name="Can change Area search intended use",
+        codename="change_intendeduse",
+    ).delete()
+
+    permission.objects.filter(
+        name="Can delete Area search intended use",
+        codename="delete_intendeduse",
     ).delete()
 
 
