488ddf442b2f9afd7c985ce2fdd192e16f5e59cb
Juho Kettunen <juho.kettunen@nordcloud.com>
2024-09-16 14:03:48 +0300

    Fix migration IDs after develop merge

--- leasing/migrations/0075_olddwellingsinhousingcompaniespriceindex_and_more.py
+++ leasing/migrations/0075_olddwellingsinhousingcompaniespriceindex_and_more.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("leasing", "0073_intendeduse_service_unit"),
+        ("leasing", "0074_delete_duplicate_intended_use_permissions"),
     ]
 
     operations = [
