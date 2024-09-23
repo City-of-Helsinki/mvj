9a51f4210ee1844df6394de5322e6e840cc791b1
Jukka Ahonen <jukka.ahonen@nordcloud.com>
2024-09-23 10:41:15 +0000

    change implementation of migration file

--- leasing/migrations/0076_paikkatietovipunen_vuokraalueet_view_vuokraustunnus_and_tyypin_tunnus.py
+++ leasing/migrations/0076_paikkatietovipunen_vuokraalueet_view_vuokraustunnus_and_tyypin_tunnus.py
@@ -3,39 +3,10 @@ from pathlib import Path
 from django.db import migrations
 
 
-class RunSQLFromFile(migrations.RunSQL):
-    def __init__(self, sql_file, reverse_sql_file=None, **kwargs):
-        super().__init__(sql="", **kwargs)
-        self.sql_file = sql_file
-        self.reverse_sql_file = reverse_sql_file
-        self._sql = "unread"
-        self._reverse_sql = "unread"
-
-    @property
-    def sql(self):
-        if self._sql == "unread":
-            self._sql = self._read_file(self.sql_file)
-        return self._sql
-
-    @sql.setter
-    def sql(self, value):
-        pass
-
-    @property
-    def reverse_sql(self):
-        if self._reverse_sql == "unread":
-            self._reverse_sql = self._read_file(self.reverse_sql_file)
-        return self._reverse_sql
-
-    @reverse_sql.setter
-    def reverse_sql(self, value):
-        pass
-
-    def _read_file(self, filename):
-        if filename is None:
-            return None
-        with open(Path(__file__).parent / filename, encoding="utf8") as fp:
-            return fp.read()
+def load_sql_file(name):
+    file_path = Path(__file__).resolve().parent / name
+    with open(file_path, encoding="utf-8") as file:
+        return file.read()
 
 
 class Migration(migrations.Migration):
@@ -44,8 +15,12 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        RunSQLFromFile(
-            sql_file="./0076_paikkatietovipunen_vuokraalueet_view_vuokraustunnus_and_tyypin_tunnus.sql",
-            reverse_sql_file="./0060_fix_paikkatietovipunen_vuokraalueet_remove_intended_use_requirement.sql",
+        migrations.RunSQL(
+            sql_file=load_sql_file(
+                "./0076_paikkatietovipunen_vuokraalueet_view_vuokraustunnus_and_tyypin_tunnus.sql"
+            ),
+            reverse_sql_file=load_sql_file(
+                "./0060_fix_paikkatietovipunen_vuokraalueet_remove_intended_use_requirement.sql"
+            ),
         ),
     ]
