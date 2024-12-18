de943dd3b13dc7a0383e87affee4b1852e78692a
Henri Nieminen <henri.nieminen@nordcloud.com>
2024-12-18 12:52:13 +0000

    add more test cases to cover set_line_item_common_values

--- laske_export/tests/test_invoice_sales_order_adapter.py
+++ laske_export/tests/test_invoice_sales_order_adapter.py
@@ -5,11 +5,13 @@ from laske_export.document.invoice_sales_order_adapter import (
 )
 from laske_export.document.sales_order import LineItem
 from leasing.models.invoice import InvoiceRow
+from leasing.models.lease import Lease, LeaseType
 from leasing.models.receivable_type import ReceivableType
+from leasing.models.service_unit import ServiceUnit
 
 
 @pytest.mark.django_db
-def test_set_line_item_common_values_sap(akv_lacking_test_setup):
+def test_set_line_item_common_values_sap_receivable_type(akv_lacking_test_setup):
     """Verifies that only one of 'wbs_element' and 'order_item_number' is set.
     If 'sap_project_number' is set, it should be set to 'wbs_element'.
     When 'sap_project_number' is set, 'sap_order_item_number' should be ignored.
@@ -45,3 +47,85 @@ def test_set_line_item_common_values_sap(akv_lacking_test_setup):
     adapter.set_line_item_common_values(line_item, invoice_row)
     assert line_item.wbs_element is None
     assert line_item.order_item_number == "456"
+
+
+@pytest.mark.django_db
+def test_set_line_item_common_values_sap_lease_type(akv_lacking_test_setup):
+    """Tests logic for handling values from LeaseType."""
+    adapter: AkvInvoiceSalesOrderAdapter = akv_lacking_test_setup["adapter"]
+    invoice_row: InvoiceRow = akv_lacking_test_setup["invoicerow1"]
+    receivable_type: ReceivableType = invoice_row.receivable_type
+    lease_type: LeaseType = invoice_row.invoice.lease.type
+    invoice_service_unit: ServiceUnit = invoice_row.invoice.service_unit
+    invoice_service_unit.default_receivable_type_rent = receivable_type
+    invoice_service_unit.save()
+
+    # Lease.internal_order is not set, ReceivableType does not have SAP values set.
+    # Use LeaseType.sap_project_number
+    lease_type.sap_project_number = "project_nr"
+    lease_type.sap_order_item_number = None
+    lease_type.save()
+    line_item = LineItem()
+    adapter.set_line_item_common_values(line_item, invoice_row)
+    assert line_item.wbs_element == "project_nr"
+    assert line_item.order_item_number is None
+
+    # Lease.internal_order is not set, ReceivableType does not have SAP values set.
+    # Use LeaseType.sap_project_number when also sap_order_item_number is set.
+    lease_type.sap_project_number = "project_nr"
+    lease_type.sap_order_item_number = "ord_nr"
+    lease_type.save()
+    line_item = LineItem()
+    adapter.set_line_item_common_values(line_item, invoice_row)
+    assert line_item.wbs_element == "project_nr"
+    assert line_item.order_item_number is None
+
+    # Lease.internal_order is not set, ReceivableType does not have SAP values set.
+    # Use LeaseType.sap_order_item_number
+    lease_type.sap_project_number = None
+    lease_type.sap_order_item_number = "ord_nr"
+    lease_type.save()
+    line_item = LineItem()
+    adapter.set_line_item_common_values(line_item, invoice_row)
+    assert line_item.wbs_element is None
+    assert line_item.order_item_number == "ord_nr"
+
+
+@pytest.mark.django_db
+def test_set_line_item_common_values_sap_lease_internal_order(akv_lacking_test_setup):
+    """Tests logic for handling case when `Lease.internal_order` is set,
+    which is expected to overwrite values when the conditions are right.
+    """
+    adapter: AkvInvoiceSalesOrderAdapter = akv_lacking_test_setup["adapter"]
+    invoice_row: InvoiceRow = akv_lacking_test_setup["invoicerow1"]
+    receivable_type: ReceivableType = invoice_row.receivable_type
+    lease: Lease = invoice_row.invoice.lease
+    lease_type: LeaseType = invoice_row.invoice.lease.type
+    invoice_service_unit: ServiceUnit = invoice_row.invoice.service_unit
+    invoice_service_unit.default_receivable_type_rent = receivable_type
+    invoice_service_unit.save()
+
+    # Lease.internal_order is set, ReceivableType does not have SAP values set.
+    # Expect line_item.order_item_number to be set to Lease.internal_order
+    lease_type.sap_project_number = None
+    lease_type.sap_order_item_number = "o_nr_notused"
+    lease_type.save()
+    lease.internal_order = "internal_ord"
+    lease.save()
+    line_item = LineItem()
+    adapter.set_line_item_common_values(line_item, invoice_row)
+    assert line_item.wbs_element is None
+    assert line_item.order_item_number == "internal_ord"
+
+    # Lease.internal_order is set, ReceivableType does not have SAP values set.
+    # LeaseType.sap_project_number is set but overwritten, and wbs_element is not set.
+    # Expect line_item.order_item_number to be set to Lease.internal_order
+    lease_type.sap_project_number = "pr_nr_not_used"
+    lease_type.sap_order_item_number = None
+    lease_type.save()
+    lease.internal_order = "internal_ord"
+    lease.save()
+    line_item = LineItem()
+    adapter.set_line_item_common_values(line_item, invoice_row)
+    assert line_item.wbs_element is None
+    assert line_item.order_item_number == "internal_ord"
