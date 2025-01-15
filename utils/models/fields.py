adc3699420b98971307b24037864303fa11f33cf
Jukka Ahonen <jukka.ahonen@nordcloud.com>
2025-01-15 10:12:26 +0000

    remove kwargs

--- utils/models/fields.py
+++ utils/models/fields.py
@@ -6,7 +6,7 @@ from django.db import models
 
 
 class PrivateFileSystemStorage(FileSystemStorage):
-    def __init__(self, **kwargs: Any) -> None:
+    def __init__(self) -> None:
         # base_url is not needed, but it defaults to MEDIA_URL if not explicitly set
         super().__init__(location=settings.PRIVATE_FILES_LOCATION, base_url=None)
 
