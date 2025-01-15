85591c1f5db66f297be256dfef025722c844882e
Jukka Ahonen <jukka.ahonen@nordcloud.com>
2025-01-15 10:06:27 +0000

    refactor PrivateFileSystemStorage

--- utils/models/fields.py
+++ utils/models/fields.py
@@ -5,13 +5,13 @@ from django.core.files.storage import FileSystemStorage
 from django.db import models
 
 
-class PrivateFileSystemStorage(FileSystemStorage): ...  # noqa: E701
+class PrivateFileSystemStorage(FileSystemStorage):
+    def __init__(self, **kwargs: Any) -> None:
+        # base_url is not needed, but it defaults to MEDIA_URL if not explicitly set
+        super().__init__(location=settings.PRIVATE_FILES_LOCATION, base_url=None)
 
 
 class PrivateFileField(models.FileField):
     def __init__(self, **kwargs: Any) -> None:
         super().__init__(**kwargs)
-        self.storage = PrivateFileSystemStorage(
-            location=settings.PRIVATE_FILES_LOCATION,
-            base_url=None,
-        )
+        self.storage = PrivateFileSystemStorage()
