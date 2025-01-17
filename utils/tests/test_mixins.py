6eb8c621ab8ee5659630d41a44d1a9c9ecc66a7d
Henri Nieminen <henri.nieminen@nordcloud.com>
2025-01-17 08:44:35 +0000

    add file max size validation

--- utils/tests/test_mixins.py
+++ utils/tests/test_mixins.py
@@ -5,7 +5,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.utils.datastructures import MultiValueDict
 from rest_framework.exceptions import ValidationError
 
-from utils.viewsets.mixins import FileExtensionFileMixin
+from utils.viewsets.mixins import MAX_FILE_SIZE_BYTES, FileExtensionFileMixin
 
 
 @pytest.fixture
@@ -193,6 +193,118 @@ def test_file_extension_mixin_update_with_invalid_files(mixin):
             "file": [
                 SimpleUploadedFile(
                     "test.exe", b"file_content", content_type="application/octet-stream"
+                )
+            ]
+        }
+    )
+    with pytest.raises(ValidationError):
+        mixin.update(request)
+
+
+def test_validate_file_size_valid(mixin):
+    bytes_under_max = b"a" * (MAX_FILE_SIZE_BYTES - 1)
+    files = MultiValueDict(
+        {
+            "file": [
+                SimpleUploadedFile(
+                    "test.pdf",
+                    bytes_under_max,
+                    content_type="application/pdf",
+                )
+            ]
+        }
+    )
+    try:
+        mixin._validate_file_size(files)
+    except ValidationError:
+        pytest.fail("_validate_file_size() raised ValidationError unexpectedly!")
+
+
+def test_validate_file_size_exceeds_limit(mixin):
+    bytes_over_max = b"a" * (MAX_FILE_SIZE_BYTES + 1)
+    files = MultiValueDict(
+        {
+            "file": [
+                SimpleUploadedFile(
+                    "test.pdf",
+                    bytes_over_max,
+                    content_type="application/pdf",
+                )
+            ]
+        }
+    )
+    with pytest.raises(ValidationError):
+        mixin._validate_file_size(files)
+
+
+def test_file_extension_mixin_create_with_valid_file_size(mixin):
+    bytes_under_max = b"a" * (MAX_FILE_SIZE_BYTES - 1)
+    request = MagicMock()
+    request.FILES = MultiValueDict(
+        {
+            "file": [
+                SimpleUploadedFile(
+                    "test.pdf",
+                    bytes_under_max,
+                    content_type="application/pdf",
+                )
+            ]
+        }
+    )
+    mixin.create = MagicMock(return_value="created")
+    response = mixin.create(request)
+    mixin.create.assert_called_once_with(request)
+    assert response == "created"
+
+
+def test_file_extension_mixin_create_with_too_large_file_size(mixin):
+    bytes_over_max = b"a" * (MAX_FILE_SIZE_BYTES + 1)
+    request = MagicMock()
+    request.FILES = MultiValueDict(
+        {
+            "file": [
+                SimpleUploadedFile(
+                    "test.pdf",
+                    bytes_over_max,
+                    content_type="application/pdf",
+                )
+            ]
+        }
+    )
+    with pytest.raises(ValidationError):
+        mixin.create(request)
+
+
+def test_file_extension_mixin_update_with_valid_file_size(mixin):
+    bytes_under_max = b"a" * (MAX_FILE_SIZE_BYTES - 1)
+    request = MagicMock()
+    request.FILES = MultiValueDict(
+        {
+            "file": [
+                SimpleUploadedFile(
+                    "test.pdf",
+                    bytes_under_max,
+                    content_type="application/pdf",
+                )
+            ]
+        }
+    )
+    mixin.update = MagicMock(return_value="updated")
+    response = mixin.update(request)
+    mixin.update.assert_called_once_with(request)
+    assert response == "updated"
+
+
+def test_file_extension_mixin_update_with_invalid_file_size(mixin):
+    bytes_over_max = b"a" * (MAX_FILE_SIZE_BYTES + 1)
+    request = MagicMock()
+    request.FILES = MultiValueDict(
+        {
+            "file": [
+                SimpleUploadedFile(
+                    "test.pdf",
+                    bytes_over_max,
+                    content_type="application/pdf",
                 )
             ]
         }
