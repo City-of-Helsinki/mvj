3440b5db113be0c46483ce3e0eac2910ac70e117
Henri Nieminen <henri.nieminen@nordcloud.com>
2025-01-22 14:13:11 +0000

    add admin for FileScanStatus model

--- filescan/admin.py
+++ filescan/admin.py
@@ -1,24 +1,25 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# from .models import FileScanStatus
+from .models import FileScanStatus
 
 
-# @admin.register(FileScanStatus)
-# class FileScanStatusAdmin(admin.ModelAdmin):
-#     list_display = (
-#         "filename",
-#         "content_type",
-#         "object_id",
-#         "scanned_at",
-#         "deleted_at",
-#         "error_message",
-#         "scan_result",
-#     )
-#     search_fields = ("filename",)
-#     list_filter = ("scanned_at", "deleted_at", "content_type", "error_message")
-#     readonly_fields = ("scanned_at", "deleted_at", "error_message", "scan_result")
+@admin.register(FileScanStatus)
+class FileScanStatusAdmin(admin.ModelAdmin):
+    list_display = (
+        "filepath",
+        "scanned_at",
+        "file_deleted_at",
+        "error_message",
+        "scan_result",
+    )
+    search_fields = ("filepath",)
+    list_filter = ("scanned_at", "file_deleted_at", "content_type", "error_message")
 
-#     def scan_result(self, scan_status: FileScanStatus) -> str:
-#         return scan_status.scan_result().value
+    # Dynamically set all fields as read-only
+    def get_readonly_fields(self, request, obj=None):
+        return [field.name for field in self.model._meta.fields] + ["scan_result"]
 
-#     scan_result.short_description = "Scan result"
+    def scan_result(self, scan_status: FileScanStatus) -> str:
+        return scan_status.scan_result().value
+
+    scan_result.short_description = "Scan result"
