0656ccd405d4a60646ed4a8b84e05559e622037f
Henri Nieminen <henri.nieminen@nordcloud.com>
2025-03-13 10:33:48 +0000

    add service_unit as a filter to VipunenMapLayer

--- leasing/models/map_layers.py
+++ leasing/models/map_layers.py
@@ -15,15 +15,22 @@ class HexColorValidator(RegexValidator):
 class VipunenMapLayer(models.Model):
     """Tree structure of categories for Vipunen map layers."""
 
-    lease_type = models.ForeignKey(
+    filter_by_lease_type = models.ForeignKey(
         "leasing.LeaseType",
         on_delete=models.SET_NULL,
         related_name="map_layers",
         null=True,
         blank=True,
     )
-    intended_use = models.ForeignKey(
+    filter_by_intended_use = models.ForeignKey(
         "leasing.IntendedUse",
+        on_delete=models.SET_NULL,
+        related_name="map_layers",
+        null=True,
+        blank=True,
+    )
+    filter_by_service_unit = models.ForeignKey(
+        "leasing.ServiceUnit",
         on_delete=models.SET_NULL,
         related_name="map_layers",
         null=True,
@@ -63,7 +70,6 @@ class VipunenMapLayer(models.Model):
         return name
 
     def get_name(self):
-
         language = get_language()
         if language == "fi" and self.name_fi is not None:
             return self.name_fi
@@ -75,14 +81,24 @@ class VipunenMapLayer(models.Model):
 
     @classmethod
     def get_map_layer_ids_for_lease_area(cls, lease_area: LeaseArea):
+        """Get all map layer ids for a lease area,
+        based on the matching attributes of LeaseType, IntendedUse and ServiceUnit."""
+
         layer_ids_qs = cls.objects.none()
         if lease_area.lease.type:
-            layer_ids_qs = cls.objects.filter(lease_type=lease_area.lease.type)
+            lease_type_qs = cls.objects.filter(lease_type=lease_area.lease.type)
+            layer_ids_qs = layer_ids_qs.union(lease_type_qs)
 
         if lease_area.lease.intended_use:
             intended_use_qs = cls.objects.filter(
                 intended_use=lease_area.lease.intended_use
             )
             layer_ids_qs = layer_ids_qs.union(intended_use_qs)
+
+        if lease_area.lease.service_unit:
+            service_unit_qs = cls.objects.filter(
+                service_unit=lease_area.lease.service_unit
+            )
+            layer_ids_qs = layer_ids_qs.union(service_unit_qs)
 
         return layer_ids_qs.values_list("id", flat=True)
