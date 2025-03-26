4baf6e28305cf02b035c09c9253c745ab551f37a
Henri Nieminen <henri.nieminen@nordcloud.com>
2025-03-26 14:25:29 +0000

    add reportstorage and lease statistics report generation
    - stores the report in jsonfield of reportstorage
    - provides API for fetching latest reportstorate for lease statistics report

---
+++ leasing/models/report_storage.py
@@ -0,0 +1,17 @@
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db import models
+
+
+class ReportStorage(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    report_data = models.JSONField(encoder=DjangoJSONEncoder)
+    report_type = models.CharField(max_length=255)
+    input_data = models.JSONField(null=True)
+
+    class Meta:
+        permissions = [
+            (
+                "export_api_lease_statistic_report",
+                "Can access export API for lease statistics report",
+            ),
+        ]
