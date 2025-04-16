ef015b1bc30b294989c4a9f6e7e8dfe237a45b1b
Juho Kettunen <juho.kettunen@nordcloud.com>
2025-04-16 15:32:46 +0300

    Send email to service unit when new areasearch is created

--- forms/tests/test_utils.py
+++ forms/tests/test_utils.py
@@ -25,9 +25,59 @@ from forms.utils import (
     generate_and_queue_answer_emails,
     send_answer_email,
 )
+from plotsearch.enums import AreaSearchLessor
 
 BASIC_TEMPLATE_SECTION_COUNT = 7
 BASIC_TEMPLATE_FIELD_COUNT = 23
+
+
+@pytest.fixture
+def setup_lessor_contacts_and_service_units(service_unit_factory, contact_factory):
+    """
+    Sets up lessor contacts and service units for testing.
+    Should be used in all tests that call generate_and_queue_answer_emails.
+
+    Normally, service units have specific known IDs, but during testing they are
+    initialized with factories, so we need to align some logic to these dynamic
+    IDs.
+    """
+    from leasing.enums import ServiceUnitId
+    from leasing.models.contact import Contact
+    from leasing.models.service_unit import ServiceUnit
+
+    # Assumption: all service units are also area search lessors
+    assert len(ServiceUnitId) == len(AreaSearchLessor)
+
+    service_units: list[ServiceUnit] = []
+    contacts: list[Contact] = []
+
+    # Instantiate service units and lessor contacts via factories
+    for unit_id in ServiceUnitId:
+        unit = service_unit_factory(name=str(unit_id))
+        service_units.append(unit)
+        contacts.append(
+            contact_factory(
+                name=unit.name,
+                service_unit=unit,
+                is_lessor=True,
+                email=f"{unit_id.name}@example.com",
+            )
+        )
+
+    def mock_map_lessor_enum_to_service_unit_id(lessor: AreaSearchLessor) -> int:
+        """Maps an areasearch lessor to a service unit ID that was created in this fixture via a factory."""
+        lessors_enum_list = list(AreaSearchLessor)
+        mock_map = {
+            lessor: service_units[i].pk for i, lessor in enumerate(lessors_enum_list)
+        }
+        return mock_map[lessor]
+
+    # Apply the mock to all tests that use this fixture
+    with patch(
+        "plotsearch.utils.map_lessor_enum_to_service_unit_id",
+        side_effect=mock_map_lessor_enum_to_service_unit_id,
+    ):
+        yield
 
 
 @pytest.mark.django_db
@@ -50,7 +100,32 @@ def test_form_cloning(basic_template_form):
 
 
 @pytest.mark.django_db
-def test_generate_and_queue_answer_emails(answer_with_email):
+def test_generate_and_send_applicant_and_lessor_emails(
+    answer_with_email, setup_lessor_contacts_and_service_units
+):
+    """Answer email should be sent to both the applicant and the lessor."""
+    answer = answer_with_email.get("answer")
+    input_data: AnswerInputData = {
+        "answer_id": answer.get("id"),
+        "answer_type": AnswerType.AREA_SEARCH,
+        "user_language": "fi",
+    }
+    with patch("forms.utils.send_answer_email") as mock_send_answer_email, patch(
+        "forms.utils._generate_applicant_plotsearch_email"
+    ) as mock_generate_applicant_plotsearch_email, patch(
+        "forms.utils._generate_lessor_new_areasearch_email"
+    ) as mock_generate_lessor_new_areasearch_email:
+        generate_and_queue_answer_emails(input_data=input_data)
+
+        mock_generate_applicant_plotsearch_email.assert_called_once()
+        mock_generate_lessor_new_areasearch_email.assert_called_once()
+        assert len(mock_send_answer_email.call_args_list) == 2
+
+
+@pytest.mark.django_db
+def test_generate_and_queue_answer_emails(
+    answer_with_email, setup_lessor_contacts_and_service_units
+):
     answer = answer_with_email.get("answer")
 
     # Case 1: Test that async_task is called, content language is Finnish
@@ -63,12 +138,12 @@ def test_generate_and_queue_answer_emails(answer_with_email):
         generate_and_queue_answer_emails(input_data=input_data)
 
         assert mock_send_answer_email.called
-        (email_message,) = mock_send_answer_email.call_args.args
-        email: EmailMessageInput = email_message
-        assert email.get("from_email") == settings.DEFAULT_FROM_EMAIL
+        (applicant_input,) = mock_send_answer_email.call_args_list[0].args
+        applicant_email: EmailMessageInput = applicant_input
+        assert applicant_email.get("from_email") == settings.DEFAULT_FROM_EMAIL
         assert (
             "Tämä on kopio Helsingin kaupungille lähetetystä aluehaun hakemuksesta."
-            in email.get("body")
+            in applicant_email.get("body")
         ), "Should contain Finnish text from the email template"
 
     # Case 1: Test that async_task is called, content language is English
@@ -81,17 +156,19 @@ def test_generate_and_queue_answer_emails(answer_with_email):
         generate_and_queue_answer_emails(input_data=input_data)
 
         assert mock_send_answer_email.called
-        (email_message,) = mock_send_answer_email.call_args.args
-        email: EmailMessageInput = email_message
-        assert email.get("from_email") == settings.DEFAULT_FROM_EMAIL
+        (applicant_input,) = mock_send_answer_email.call_args_list[0].args
+        applicant_email: EmailMessageInput = applicant_input
+        assert applicant_email.get("from_email") == settings.DEFAULT_FROM_EMAIL
         assert (
             "This is a copy of your area search application sent to the City of Helsinki."
-            in email.get("body")
+            in applicant_email.get("body")
         ), "Should contain English text from the email template"
 
 
 @pytest.mark.django_db
-def test_generate_email_user_language(answer_with_email):
+def test_generate_email_user_language(
+    answer_with_email, setup_lessor_contacts_and_service_units
+):
     answer_id = answer_with_email.get("answer", {}).get("id")
     default_user_language = "fi"
 
@@ -105,7 +182,7 @@ def test_generate_email_user_language(answer_with_email):
         }
         generate_and_queue_answer_emails(input_data=input_data)
         assert mock_override.called
-        language_code = mock_override.call_args.args[0]
+        language_code = mock_override.call_args_list[0].args[0]
         assert language_code == user_language
 
     # Case 2: User language is set to Georgian, should default to Finnish
@@ -118,7 +195,7 @@ def test_generate_email_user_language(answer_with_email):
         }
         generate_and_queue_answer_emails(input_data=input_data)
         assert mock_override.called
-        language_code = mock_override.call_args.args[0]
+        language_code = mock_override.call_args_list[0].args[0]
         assert language_code == default_user_language
 
     def get_language_side_effect(*args, **kwargs):
@@ -128,7 +205,8 @@ def test_generate_email_user_language(answer_with_email):
     # Case 3: User language is set to Swedish
     # Expecting the current language context to be Swedish
     with patch(
-        "forms.utils._generate_plotsearch_email", side_effect=get_language_side_effect
+        "forms.utils._generate_applicant_plotsearch_email",
+        side_effect=get_language_side_effect,
     ) as mock_generate_plotsearch_email:
         user_language = "sv"
         input_data: AnswerInputData = {
@@ -143,7 +221,9 @@ def test_generate_email_user_language(answer_with_email):
 
 
 # @pytest.mark.django_db
-# def test_generate_area_search_pdf_and_email_to_disk(answer_with_email):
+# def test_generate_area_search_pdf_and_email_to_disk(
+#     answer_with_email, setup_lessor_contacts_and_service_units
+# ):
 #     """This test is for debugging purposes only. It generates the email and pdf files to disk."""
 #     import os
 
@@ -156,7 +236,7 @@ def test_generate_email_user_language(answer_with_email):
 #             "user_language": "fi",
 #         }
 #         generate_and_queue_answer_emails(input_data=input_data)
-#         email: EmailMessageInput = mock_send_answer_email.call_args.args[0]
+#         email: EmailMessageInput = mock_send_answer_email.call_args_list[0].args[0]
 
 #     directory = (
 #         "/code/tmp"  # Change this to your desired path, this is for devcontainers only
@@ -168,8 +248,10 @@ def test_generate_email_user_language(answer_with_email):
 #             f.write(content)
 
 #     email_file_path = os.path.join(directory, f"{email.get('to')[0]}")
-#     with open(email_file_path, "wb") as f:
-#         f.write(email.get("body").encode("utf-8"))
+#     with open(email_file_path, "w") as f:
+#         f.write(email.get("subject"))
+#         f.write("\n------------------------------\n\n")
+#         f.write(email.get("body"))
 
 
 def test_send_answer_email(answer_email_message: EmailMessageInput):
