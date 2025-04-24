7c06925369d7274a780ea347fc3b0535b96fa77f
Juho Kettunen <juho.kettunen@nordcloud.com>
2025-04-24 15:26:16 +0300

    Migration for translations in model

--- plotsearch/migrations/0044_areasearch_settled_date.py
+++ plotsearch/migrations/0044_areasearch_settled_date.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("plotsearch", "0043_areasearchattachment_user_amr_list"),
     ]
