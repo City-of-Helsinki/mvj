2ae136fddaa070707803d8c999252c3ab8a705a9
Juho Kettunen <juho.kettunen@nordcloud.com>
2025-05-26 16:06:15 +0300

    Schedule a reminder constructability email
    The reminder is scheduled 7 days after initial email.
    7 days is the furthest currently available scheduling afforded by
    MailGun API and our billing plan.
    
    Ideal schedule would be 14 days after initial email.

--- leasing/viewsets/email.py
+++ leasing/viewsets/email.py
@@ -1,14 +1,20 @@
+from email.utils import format_datetime
+
+from dateutil.relativedelta import relativedelta
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from leasing.enums import EmailLogType
 from leasing.metadata import FieldsMetadata
 from leasing.models.email import EmailLog
 from leasing.permissions import PerMethodPermission
 from leasing.serializers.email import SendEmailSerializer
+from utils.email import EmailMessageInput
 
 
 class SendEmailView(APIView):
@@ -26,11 +32,15 @@ class SendEmailView(APIView):
         serializer = SendEmailSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
+        email_type = serializer.validated_data["type"]
+        email_body = serializer.validated_data["text"]
+        lease = serializer.validated_data["lease"]
+
         email_log = EmailLog.objects.create(
-            type=serializer.validated_data["type"],
+            type=email_type,
             user=request.user,
-            text=serializer.validated_data["text"],
-            content_object=serializer.validated_data["lease"],
+            text=email_body,
+            content_object=lease,
         )
 
         if request.user.email:
@@ -42,18 +52,21 @@ class SendEmailView(APIView):
             if not recipient.email:
                 continue
 
-            send_mail(
-                _("MVJ lease {} {}").format(
-                    serializer.validated_data["lease"].identifier,
-                    serializer.validated_data["type"],
+            email_input: EmailMessageInput = {
+                "from_email": from_email,
+                "to": [recipient.email],
+                "subject": _("MVJ lease {} {}").format(
+                    lease.identifier,
+                    email_type,
                 ),
-                serializer.validated_data["text"],
-                from_email,
-                [recipient.email],
-                fail_silently=False,
-            )
-
+                "body": email_body,
+                "attachments": [],
+            }
+            self._send_email(email_input)
             email_log.recipients.add(recipient)
+
+            if email_type == EmailLogType.CONSTRUCTABILITY:
+                self._schedule_constructability_reminder_email(email_input)
 
         result = {"sent": True}
 
@@ -68,3 +81,21 @@ class SendEmailView(APIView):
         )
 
         return Response(data, status=status.HTTP_200_OK)
+
+    def _send_email(self, email_input: EmailMessageInput) -> None:
+        email_message = EmailMessage(**email_input)
+        email_message.send()
+
+    def _schedule_constructability_reminder_email(
+        self, email_input: EmailMessageInput
+    ) -> None:
+        """
+        Constructability emails come with a reminder email.
+
+        Relies on Mailgun's API's `o:deliverytime` parameter for scheduling.
+        """
+        email_input["subject"] = _("Reminder: {}").format(email_input["subject"])
+        email = EmailMessage(**email_input)
+        reminder_time = timezone.now() + relativedelta(days=7)
+        email.esp_extra = {"o:deliverytime": format_datetime(reminder_time)}
+        email.send(fail_silently=False)
