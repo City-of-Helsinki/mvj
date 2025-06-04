b7031841d1eb5e689d6d43d8a15abeab0d50a826
Juho Kettunen <juho.kettunen@nordcloud.com>
2025-06-04 10:43:50 +0300

    Delete unnecessary commands from docker entrypoint

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -18,17 +18,10 @@ if [[ "$APPLY_MIGRATIONS" = "1" ]]; then
     ./manage.py migrate --noinput
 fi
 
-# Create superuser
-if [[ "$CREATE_SUPERUSER" = "1" ]]; then
-    ./manage.py add_admin_user -u admin -p admin -e admin@example.com
-    echo "Admin user created with credentials admin:admin (email: admin@example.com)"
-fi
 
 # Start server
 if [[ ! -z "$@" ]]; then
     "$@"
-elif [[ "$DEV_SERVER" = "1" ]]; then
-    python ./manage.py runserver 0.0.0.0:8000
 else
     uwsgi --ini deploy/uwsgi.ini
 fi
