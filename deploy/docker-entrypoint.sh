54630835457d1616baf0943bf79c20dbb4d715be
Henri Nieminen <henri.nieminen@nordcloud.com>
2025-06-24 10:56:47 +0000

    entrypoint runs migrations in initContainer

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -2,22 +2,14 @@
 
 set -e
 
-if [ -z "$SKIP_DATABASE_CHECK" -o "$SKIP_DATABASE_CHECK" = "0" ]; then
-    until nc -z -v -w30 "$DATABASE_HOST" "$DATABASE_PORT"
-    do
-      echo "Waiting for postgres database connection..."
-      sleep 1
-    done
-    echo "Database is up!"
+# Check if this is an initContainer
+if [[ ! -z "$IS_KUBERNETES_INIT_CONTAINER" ]]; then
+    echo "Running in initialization container mode"
+    # Run migrations in init container
+    bash deploy/init-migrate.sh
+    # Exit to not start uwsgi in initContainer
+    exit 0
 fi
-
-
-# Apply database migrations
-if [[ "$APPLY_MIGRATIONS" = "1" ]]; then
-    echo "Applying database migrations..."
-    ./manage.py migrate --noinput
-fi
-
 
 # Start server
 if [[ ! -z "$@" ]]; then
---
+++ deploy/init-migrate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$SKIP_DATABASE_CHECK" -o "$SKIP_DATABASE_CHECK" = "0" ]; then
+    until nc -z -v -w30 "$DATABASE_HOST" "$DATABASE_PORT"
+    do
+      echo "Waiting for postgres database connection..."
+      sleep 1
+    done
+    echo "Database is up!"
+fi
+
+
+# Apply database migrations
+if [[ "$APPLY_MIGRATIONS" = "1" ]]; then
+    echo "Applying database migrations..."
+    ./manage.py migrate --noinput
+else
+    echo "Skipping migrations..."
+fi
