c3baa9eeea4e01f3f60751945e0286c5677a9129
Henri Nieminen <henri.nieminen@nordcloud.com>
2025-07-01 10:51:47 +0000

    add arguments to docker-entrypoint
    allows reusing the same image for multiple purposes

--- deploy/docker-entrypoint.sh
+++ deploy/docker-entrypoint.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
-
 set -e
 
-# Check if this is an initContainer
-if [[ ! -z "$IS_KUBERNETES_INIT_CONTAINER" ]]; then
-    echo "Running in initialization container mode"
-    # Run migrations in init container
-    bash deploy/init-migrate.sh
-    # Exit to not start uwsgi in initContainer
-    exit 0
-fi
+echo "Entrypoint executing with args: $@"
 
-# Start server
-if [[ ! -z "$@" ]]; then
-    "$@"
-else
-    uwsgi --ini deploy/uwsgi.ini
-fi
+
+start_api() {
+    # Check if this is an initContainer
+    if [[ ! -z "$IS_KUBERNETES_INIT_CONTAINER" ]]; then
+        echo "Running in initialization container mode"
+        # Run migrations in init container
+        bash deploy/init-migrate.sh
+        # Exit to allow main container to start, and in order to not start uwsgi in initContainer
+        exit 0
+    fi
+
+    echo "Starting uWSGI server"
+    exec uwsgi --ini deploy/uwsgi.ini
+}
+
+case "$1" in
+    batchrun)
+        echo "Running batchrun scheduler"
+        exec python ./manage.py batchrun_scheduler
+        ;;
+
+    qcluster)
+        echo "Running qcluster"
+        exec python ./manage.py qcluster
+        ;;
+
+    api)
+        echo "Running api"
+        start_api
+        ;;
+
+    *)
+        echo "Unknown command: $1"
+        echo "Usage: $0 {batchrun|qcluster|api}"
+        echo "Running default command: api"
+        start_api
+        ;;
+esac
