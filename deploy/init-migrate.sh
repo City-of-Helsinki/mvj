6510ffb3104df317d95f2cf0a2186a7b28244a2b
Henri Nieminen <henri.nieminen@nordcloud.com>
2025-08-28 11:53:01 +0000

    remove database check from init-migrate
    also removes netcat as a dependency from Dockerfile
    
    it was decided that this functionality is not needed for now, and it can be brought back if need arises

--- deploy/init-migrate.sh
+++ deploy/init-migrate.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-if [ -z "$SKIP_DATABASE_CHECK" -o "$SKIP_DATABASE_CHECK" = "0" ]; then
-    until nc -z -v -w30 "$DATABASE_HOST" "$DATABASE_PORT"
-    do
-      echo "Waiting for postgres database connection..."
-      sleep 1
-    done
-    echo "Database is up!"
-fi
-
-
 # Apply database migrations
 if [[ "$APPLY_MIGRATIONS" = "1" ]]; then
     echo "Applying database migrations..."
