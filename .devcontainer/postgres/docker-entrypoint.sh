5d804a133753b1bc3c8b50aad96c7c1447ea439f
Henri Nieminen <henri.nieminen@nordcloud.com>
2025-09-23 07:28:26 +0000

    devcontainer to python11, postgres 14 with postgis 3.5

--- .devcontainer/postgres/docker-entrypoint.sh
+++ .devcontainer/postgres/docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "mvj" <<-EOSQL
+# Wait for PostgreSQL to be ready
+until pg_isready -U "$POSTGRES_USER" -d "$POSTGRES_DB" -p "$POSTGRES_PORT"; do
+  echo "Waiting for PostgreSQL to be ready..."
+  sleep 1
+done
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "mvj-db" --port "$POSTGRES_PORT" <<-EOSQL
 	CREATE EXTENSION IF NOT EXISTS postgis;
 EOSQL
