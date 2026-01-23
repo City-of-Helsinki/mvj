ce350adf808013bc0cbbd098a6bb633ed07655fd
Kimi Ihalainen <kimi.ihalainen@nordcloud.com>
2026-01-23 09:56:30 +0200

    Add collection stage and postpone date to open invoice report

--- leasing/report/invoice/open_invoices_report.py
+++ leasing/report/invoice/open_invoices_report.py
@@ -54,6 +54,17 @@ def get_lease_id(invoice: Invoice) -> int:
     return invoice.lease.id
 
 
+def get_collection_stage(invoice: Invoice) -> str | None:
+    latest_note = (
+        invoice.lease.collection_notes.filter(collection_stage__isnull=False)
+        .order_by("-created_at")
+        .first()
+    )
+
+    if latest_note:
+        return str(latest_note.collection_stage.label)
+
+
 class OpenInvoicesReport(ReportBase):
     name = _("Open invoices")
     description = _('Show all the invoices that have their state as "open"')
@@ -76,6 +87,10 @@ class OpenInvoicesReport(ReportBase):
             "format": FormatType.URL.value,
         },
         "due_date": {"label": _("Due date"), "format": FormatType.DATE.value},
+        "postpone_date": {
+            "label": _("Postpone date"),
+            "format": FormatType.DATE.value,
+        },
         "due_dates_per_year": {
             "source": get_due_dates_per_year,
             "label": _("Due dates per year"),
@@ -104,6 +119,11 @@ class OpenInvoicesReport(ReportBase):
             "source": get_recipient_address,
             "label": _("Recipient address"),
             "width": 50,
+        },
+        "collection_stage": {
+            "source": get_collection_stage,
+            "label": _("Collection stage"),
+            "width": 35,
         },
         "lease_database_id": {
             "source": get_lease_id,
@@ -159,9 +179,9 @@ class OpenInvoicesReport(ReportBase):
             totals_row.cells.append(
                 ExcelCell(column=0, value="{} {}".format(lease_type, _("Total")))
             )
-            totals_row.cells.append(PreviousRowsSumCell(column=4, count=invoice_count))
             totals_row.cells.append(PreviousRowsSumCell(column=5, count=invoice_count))
             totals_row.cells.append(PreviousRowsSumCell(column=6, count=invoice_count))
+            totals_row.cells.append(PreviousRowsSumCell(column=7, count=invoice_count))
             result.append(totals_row)
             totals_row_nums.append(data_row_num)
 
@@ -170,18 +190,18 @@ class OpenInvoicesReport(ReportBase):
         totals_row = ExcelRow()
         totals_row.cells.append(ExcelCell(column=0, value=str(_("Grand total"))))
 
-        total_amount_sum_cell = SumCell(column=4)
-        billed_amount_sum_cell = SumCell(column=5)
-        outstanding_amount_sum_cell = SumCell(column=6)
+        total_amount_sum_cell = SumCell(column=5)
+        billed_amount_sum_cell = SumCell(column=6)
+        outstanding_amount_sum_cell = SumCell(column=7)
         for totals_row_num in totals_row_nums:
             total_amount_sum_cell.add_target_range(
-                (totals_row_num, 4, totals_row_num, 4)
-            )
-            billed_amount_sum_cell.add_target_range(
                 (totals_row_num, 5, totals_row_num, 5)
             )
-            outstanding_amount_sum_cell.add_target_range(
+            billed_amount_sum_cell.add_target_range(
                 (totals_row_num, 6, totals_row_num, 6)
+            )
+            outstanding_amount_sum_cell.add_target_range(
+                (totals_row_num, 7, totals_row_num, 7)
             )
 
         totals_row.cells.append(total_amount_sum_cell)
