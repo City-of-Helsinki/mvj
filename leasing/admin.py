073fd69caca4348717d526e2b8ee99e74dfc9dee
Kimmo <kimmo@netzionale.com>
2026-03-10 12:02:51 +0200

    add search to inspections

--- leasing/admin.py
+++ leasing/admin.py
@@ -343,6 +343,7 @@ class ConditionInline(FieldPermissionsAdminMixin, admin.StackedInline):
 @admin.register(Decision)
 class DecisionAdmin(FieldPermissionsModelAdmin):
     list_display = ("lease", "reference_number", "decision_maker", "type")
+    search_fields = ["reference_number", "decision_maker__name","lease__identifier__identifier"]
     inlines = [ConditionInline]
     raw_id_fields = ("lease",)
 
@@ -370,6 +371,7 @@ class DecisionTypeAdmin(NameAdmin):
 @admin.register(Inspection)
 class InspectionAdmin(FieldPermissionsModelAdmin):
     list_display = ("lease", "inspector", "supervision_date", "supervised_date")
+    search_fields = ["lease__identifier__identifier", "inspector__name"]
     raw_id_fields = ("lease",)
 
     def get_queryset(self, request):
