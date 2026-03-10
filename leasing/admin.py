e786219c498ab8878d957dc6b068e3592a5fa036
Kimmo <kimmo@netzionale.com>
2026-03-10 11:58:47 +0200

    add list search and stage filter to CollectionNote

--- leasing/admin.py
+++ leasing/admin.py
@@ -252,6 +252,8 @@ class CollectionLetterAdmin(FieldPermissionsModelAdmin):
 
 class CollectionNoteAdmin(FieldPermissionsModelAdmin):
     list_display = ("lease", "created_at", "note", "user")
+    search_fields = ["lease__identifier", "user__first_name", "user__last_name",]
+    list_filter = ("collection_stage")
     raw_id_fields = ("lease",)
     ordering = ("-created_at",)
 
