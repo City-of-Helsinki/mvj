ecedeebb809e5f4d1c878dbee9bce6b0a7c12136
Kimmo <kimmo@netzionale.com>
2026-03-23 13:26:38 +0200

    add search and filter to LeaseArea & Plot admins

--- leasing/admin.py
+++ leasing/admin.py
@@ -649,6 +649,8 @@ class LeaseAreaAddressInline(FieldPermissionsAdminMixin, admin.TabularInline):
 
 class LeaseAreaAdmin(FieldPermissionsModelAdmin):
     list_display = ("lease", "identifier", "type")
+    search_fields = ["lease__identifier__identifier", "identifier"]
+    list_filter = ("type",)
     inlines = [
         LeaseAreaAddressInline,
         ConstructabilityDescriptionInline,
@@ -673,6 +675,8 @@ class LeaseAreaAdmin(FieldPermissionsModelAdmin):
 
 class PlotAdmin(FieldPermissionsModelAdmin):
     list_display = ("get_lease_area_identifier", "type")
+    search_fields = ["lease_area__identifier", "type"]
+    list_filter = ("type",)
     raw_id_fields = ("lease_area",)
 
     def get_lease_area_identifier(self, obj):
