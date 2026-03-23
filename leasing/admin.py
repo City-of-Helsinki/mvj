cafcf074b4c9baeeb6d7538afd36cd2f4c2c9c12
Henri Nieminen <henri.nieminen@nordcloud.com>
2026-03-23 08:59:47 +0000

    fix getting tenant names in TenantAdmin & getting type in PlanUnitadmin

--- leasing/admin.py
+++ leasing/admin.py
@@ -182,7 +182,7 @@ class TenantContactInline(FieldPermissionsAdminMixin, admin.TabularInline):
 
 
 class TenantAdmin(FieldPermissionsModelAdmin):
-    list_display = ("lease", "tenantcontact__contact__name")
+    list_display = ("lease", "get_tenant_names")
     search_fields = [
         "lease__identifier__identifier",
     ]
@@ -200,7 +200,12 @@ class TenantAdmin(FieldPermissionsModelAdmin):
             "lease__identifier__type",
             "lease__identifier__municipality",
             "lease__identifier__district",
-        )
+        ).prefetch_related("contacts")
+
+    @admin.display(description=_("Tenant names"))
+    def get_tenant_names(self, obj: Tenant):
+        tenant_names: list[str] = [contact.get_name() for contact in obj.contacts.all()]
+        return " / ".join(tenant_names) or "-"
 
 
 class RelatedLeaseInline(FieldPermissionsAdminMixin, admin.TabularInline):
@@ -697,7 +702,7 @@ class LeaseStateLogAdmin(admin.ModelAdmin):
 
 class PlanUnitAdmin(FieldPermissionsModelAdmin):
     list_display = ("identifier", "get_lease_identifier", "lease_area")
-    list_filter = ("type__name",)
+    list_filter = ("plan_unit_type__name",)
     raw_id_fields = ("lease_area",)
     search_fields = ["identifier"]
 
