460471dc3449d4e8df90a6f14ff25789557e92ff
Henri Nieminen <henri.nieminen@nordcloud.com>
2026-04-24 08:52:21 +0000

    remove undefined and unused enable_signals decorator from tests
    - `@pytest.mark.enable_signals` had no effect since it is not defined anywhere in the codebase nor in any dependency

--- plotsearch/tests/api/test_information_check.py
+++ plotsearch/tests/api/test_information_check.py
@@ -8,7 +8,6 @@ fake = Faker("fi_FI")
 
 
 @pytest.mark.django_db
-@pytest.mark.enable_signals
 def test_information_check(admin_client, admin_user, basic_answer):
     url = reverse("v1:informationcheck-list")
 
