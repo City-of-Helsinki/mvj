6af1f4984b655a06cc8970e9ecf76dba3d2e568d
Kimi Ihalainen <kimi.ihalainen@nordcloud.com>
2026-06-29 13:51:22 +0000

    Add field formatting to report serializer

--- leasing/report/serializers.py
+++ leasing/report/serializers.py
@@ -1,8 +1,12 @@
 import copy
 from collections import OrderedDict
+from decimal import ROUND_HALF_UP, Decimal
 
+from django.utils import formats
 from rest_framework import serializers
 from rest_framework.fields import Field
+
+from leasing.report.excel import FormatType
 
 
 class ReportOutputSerializer(serializers.Serializer):
@@ -33,8 +37,20 @@ class ReportOutputSerializer(serializers.Serializer):
                 continue
 
             field_source = field_attrs.get("source", None)
+            format_type = field_attrs.get("format", None)
 
-            if field_source is None:
+            if format_type in [
+                FormatType.MONEY.value,
+                FormatType.NUMBER.value,
+                FormatType.PERCENTAGE.value,
+                FormatType.AREA.value,
+            ]:
+                if callable(field_source):
+                    setattr(self, "get_{}".format(field_name), field_source)
+                    fields[field_name] = FormattedDecimalMethodField()
+                else:
+                    fields[field_name] = FormattedDecimalField(source=field_source)
+            elif field_source is None:
                 fields[field_name] = serializers.ReadOnlyField()
             elif callable(field_source):
                 setattr(self, "get_{}".format(field_name), field_source)
@@ -43,3 +59,39 @@ class ReportOutputSerializer(serializers.Serializer):
                 fields[field_name] = serializers.ReadOnlyField(source=field_source)
 
         return fields
+
+
+def _format_decimal_value(value):
+    if value is None or value == "":
+        return value
+
+    if not isinstance(value, Decimal):
+        try:
+            value = Decimal(str(value))
+        except (ValueError, TypeError):
+            return value
+
+    return formats.number_format(
+        value.quantize(Decimal(".01"), rounding=ROUND_HALF_UP),
+        decimal_pos=2,
+        use_l10n=True,
+    )
+
+
+class FormattedDecimalField(serializers.Field):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def to_representation(self, value):
+        return _format_decimal_value(value)
+
+
+class FormattedDecimalMethodField(serializers.SerializerMethodField):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def to_representation(self, value):
+        result = super().to_representation(value)
+        return _format_decimal_value(result)
