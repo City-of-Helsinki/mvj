07fc5a99ede05abe74ffc7581ac26590dee05e4e
Kimi Ihalainen <kimi.ihalainen@nordcloud.com>
2026-07-15 11:14:59 +0000

    Add test for business id lease filtering

--- leasing/tests/api/test_lease_search.py
+++ leasing/tests/api/test_lease_search.py
@@ -1,5 +1,9 @@
+import datetime
+
 import pytest
 from django.urls import reverse
+
+from leasing.enums import ContactType, TenantContactType
 
 
 @pytest.mark.django_db
@@ -43,3 +47,60 @@ def test_search_finds_one_lease_by_full_identifier_where_type_has_two_letters(
 
     assert response.data["count"] == 1
     assert response.data["results"][0]["id"] == lease.id
+
+
+@pytest.mark.parametrize(
+    "tenantcontact_type, tenant_activity, expected_result_count",
+    [
+        ("", "all", 1),
+        (TenantContactType.TENANT.value, "active", 1),
+        (TenantContactType.BILLING.value, "all", 0),
+        ("", "past", 0),
+    ],
+)
+@pytest.mark.django_db
+def test_business_id_affects_tenantcontact_type_and_tenant_activity_filters(
+    django_db_setup,
+    admin_client,
+    lease_factory,
+    tenant_factory,
+    tenant_contact_factory,
+    contact_factory,
+    tenantcontact_type,
+    tenant_activity,
+    expected_result_count,
+):
+    business_id = "1234567-8"
+    lease = lease_factory(
+        type_id=1, municipality_id=1, district_id=1, notice_period_id=1
+    )
+    tenant = tenant_factory(
+        lease=lease,
+        share_numerator=1,
+        share_denominator=1,
+    )
+    contact = contact_factory(
+        first_name="First name",
+        last_name="Last name",
+        business_id=business_id,
+        type=ContactType.BUSINESS,
+    )
+    start_date = datetime.date(2000, 1, 1)
+    tenant_contact_factory(
+        type=TenantContactType.TENANT,
+        tenant=tenant,
+        contact=contact,
+        start_date=start_date,
+    )
+
+    response = admin_client.get(
+        reverse("v1:lease-list"),
+        data={
+            "business_id": business_id,
+            "tenantcontact_type": tenantcontact_type,
+            "tenant_activity": tenant_activity,
+        },
+    )
+
+    assert response.status_code == 200
+    assert response.data["count"] == expected_result_count
